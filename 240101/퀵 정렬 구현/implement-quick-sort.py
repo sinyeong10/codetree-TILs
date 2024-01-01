2c2d14d7c88609f93b2ab3be40256f57601e6b10
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-01 19:00:24 +0900

    Title: 퀵 정렬 구현, Time: 311ms, Memory: 31MB, Status: Passed - CodeTree

--- 240101/퀵 정렬 구현/implement-quick-sort.py
+++ 240101/퀵 정렬 구현/implement-quick-sort.py
@@ -3,14 +3,16 @@ n = int(stdin.readline())
 base = list(map(int, stdin.readline().split()))
 
 def quick_sorting(base, low, high):
-    pivot = base[low]
-    i = low
+    pivot = base[low] #처음이 pivot
+    i = low #이후 pivot보다 작은 값이 나올 시 바꿀 위치
     
+    #순회하며 작은 값 나올시 바꿈
     for j in range(low+1, high+1): #low+1~high 비교대상
         if base[j] < pivot:
-            i += 1
+            i += 1 #다음 값을 큰값으로 생각, i전까지 작은 값으로 되어있다
             base[i], base[j] = base[j], base[i]
 
+    #pivot을 중앙 기준으로 옮기고 기준점 반환
     base[i], base[low] = base[low], base[i]
     return i    
 
