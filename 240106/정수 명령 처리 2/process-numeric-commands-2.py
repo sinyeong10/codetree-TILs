b2e44bf43aedcc4f9ef98e0cd20f5a0240d11339
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-06 20:13:35 +0900

    Title: 정수 명령 처리 2, Time: 281ms, Memory: 28MB, Status: Passed - CodeTree

--- 240106/정수 명령 처리 2/process-numeric-commands-2.py
+++ 240106/정수 명령 처리 2/process-numeric-commands-2.py
@@ -1,42 +1,60 @@
-from collections import deque
+# from collections import deque
 
-class Queue:
-    def __init__(self):
-        self.base = deque()
+# class Queue:
+#     def __init__(self):
+#         self.base = deque()
     
-    def push(self, A):
-        self.base.append(A)
+#     def push(self, A):
+#         self.base.append(A)
     
-    def empty(self):
-        return not self.base
+#     def empty(self):
+#         return not self.base
     
-    def size(self):
-        return len(self.base)
+#     def size(self):
+#         return len(self.base)
     
-    def pop(self):
-        if self.empty():
-            raise Exception("empty")
-        return self.base.popleft()
+#     def pop(self):
+#         if self.empty():
+#             raise Exception("empty")
+#         return self.base.popleft()
     
-    def front(self):
-        if self.empty():
-            raise Exception("empty")
-        return self.base[0]
+#     def front(self):
+#         if self.empty():
+#             raise Exception("empty")
+#         return self.base[0]
     
+
+# from sys import stdin
+# n = int(stdin.readline())
+# base = Queue()
+# for _ in range(n):
+#     order = stdin.readline().split()
+#     # print(order)
+#     if order[0] == "push":
+#         base.push(order[1])
+#     elif order[0] == "front":
+#         print(base.front())
+#     elif order[0] == "size":
+#         print(base.size())
+#     elif order[0] == "pop":
+#         print(base.pop())
+#     elif order[0] == "empty":
+#         print(1 if base.empty() else 0)
+
+from collections import deque
 
 from sys import stdin
 n = int(stdin.readline())
-base = Queue()
+base = deque()
 for _ in range(n):
     order = stdin.readline().split()
-    # print(order)
     if order[0] == "push":
-        base.push(order[1])
+        base.append(order[1])
     elif order[0] == "front":
-        print(base.front())
+        print(base[0])
     elif order[0] == "size":
-        print(base.size())
+        print(len(base))
     elif order[0] == "pop":
-        print(base.pop())
+        print(base.popleft())
     elif order[0] == "empty":
-        print(1 if base.empty() else 0)
+        print(1 if not base else 0)
