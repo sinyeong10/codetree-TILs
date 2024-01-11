643780d6cdabb0c767c72ac19224265f2239f9c4
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-12 00:15:26 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240112/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -0,0 +1,35 @@
+from sys import stdin
+n, m, t = list(map(int, stdin.readline().split()))
+base = [list(map(int, stdin.readline().split())) for _ in range(n)]
+ball = [list(map(int, stdin.readline().split())) for _ in range(m)]
+
+def in_range(x, y):
+    return 0<=x<n and 0<=y<n
+
+def move():
+    global ball
+    tmp = [[0]*n for _ in range(n)]
+    next_ball = []
+
+    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1] #상하좌우
+
+    for i in range(m):
+        x, y = ball[i]
+        next_pos = [-1, -1]
+        value_max = 0
+        for dxs, dys in zip(dx, dy):
+            next_x, next_y = x-1+dxs, y-1+dys
+            if in_range(next_x, next_y) and base[next_x][next_y] > value_max:
+                value_max = base[next_x][next_y]
+                next_pos = [next_x, next_y]
+        tmp[next_pos[0]][next_pos[1]] += 1
+    
+    for i in range(n):
+        for j in range(n):
+            if tmp[i][j] == 1:
+                next_ball.append([i, j])
+    ball = next_ball
+
+for _ in range(t):
+    move()
+print(len(ball))
