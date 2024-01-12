b7a45c5425b83f28a9ca835c10898f0de2dc702a
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-12 20:29:41 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 192ms, Memory: 26MB, Status: Passed - CodeTree

--- 240112/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 240112/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -18,13 +18,16 @@ def bfs():
 
     while q:
         x, y = q.popleft() #deque에서 left를 붙일 시 처음 의미!
-        visited[x][y] = True
+        # print(x,y)
+        # visited[x][y] = True #q에 넣고 중복처리를 q에서 빼낼 때 하면 중복이 발생!
         for dxs, dys in zip(dx, dy):
             next_x, next_y = x+dxs, y+dys
             if can_go(next_x, next_y):
-                q.append([next_x, next_y])
+                q.append([next_x, next_y]) #q에 넣고 중복처리를 q에서 빼낼 때 하면 중복이 발생!
+                visited[next_x][next_y] = True
 
 q.append([0,0])
+visited[0][0] = True #q에서 뺄때가 아니라 넣을 때 처리!
 bfs()
 
 # for elem in visited:
