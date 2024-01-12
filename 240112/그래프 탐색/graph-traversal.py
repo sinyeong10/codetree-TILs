b258e9736fe99f989fbb875426452ea88fd35dc8
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-12 18:51:26 +0900

    Title: 그래프 탐색, Time: 139ms, Memory: 26MB, Status: Passed - CodeTree

--- 240112/그래프 탐색/graph-traversal.py
+++ 240112/그래프 탐색/graph-traversal.py
@@ -9,16 +9,27 @@ for _ in range(m):
 
 visited = [False for _ in range(n+1)]
 
+# def dfs(node): #현재 방문 노드!
+#     total = 0 #현재 노드의 자식의 총합
+#     visited[node] = True
+#     # print(node)
+#     for next_node in graph[node]:
+#         if visited[next_node]:
+#             continue
+#         # print("이동", next_node)
+#         total += 1 #다음 노드
+#         total += dfs(next_node) #for문 돌며 자식의 경우를 다 더함
+#     return total #자식 다 더한 결과를 부모에 전달
+#print(dfs(1)) #여기선 처음 시작노드를 제외하고 카운트됨
+
 def dfs(node): #현재 방문 노드!
-    total = 0 #현재 노드의 자식의 총합
+    total = 1 #현재 노드와 자식의 총합
     visited[node] = True
-    # print(node)
     for next_node in graph[node]:
         if visited[next_node]:
             continue
-        # print("이동", next_node)
-        total += 1 #다음 노드
+        # total += 1 #다음 노드를 여기선 체크안해도 됨!
         total += dfs(next_node) #for문 돌며 자식의 경우를 다 더함
     return total #자식 다 더한 결과를 부모에 전달
 
-print(dfs(1))
+print(dfs(1)-1) #1번 노드 제외
