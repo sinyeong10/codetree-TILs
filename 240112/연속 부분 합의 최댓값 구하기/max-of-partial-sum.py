71da57ea885a8c5c455b9902ef5b95d493576a88
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-12 17:52:50 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240112/연속 부분 합의 최댓값 구하기/max-of-partial-sum.py
@@ -0,0 +1,13 @@
+from sys import stdin
+n = int(stdin.readline())
+base = list(map(int, stdin.readline().split()))
+
+dp = [float("-inf") for _ in range(n)] #-float("inf")는 float("-inf")
+# print(dp)
+
+dp[0] = base[0]
+
+for i in range(1, n):
+    dp[i] = max(base[i], dp[i-1]+base[i])
+
+print(dp[-1])
