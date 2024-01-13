8d9925e4228d6c2904bcc48c7efb6e16e6589e66
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-13 20:21:43 +0900

    Title: 적절한 옷 고르기, Time: 392ms, Memory: 26MB, Status: Passed - CodeTree

--- 240113/적절한 옷 고르기/select-proper-clothes.py
+++ 240113/적절한 옷 고르기/select-proper-clothes.py
@@ -13,7 +13,7 @@ for i in range(n):
 for i in range(2, m+1): #2~m번째 일의 계산
     for j in range(n): #n개의 옷 중 마지막으로 j번째 옷을 입는 경우
         s,e,v = data[j]
-        if s<=i<=e:
+        if s<=i<=e: #해당 날짜에 옷을 입을 수 있는 경우 이전을 체크하여 갱신
             max_value = 0
             for k in range(n): #이전 체크
                 if dp[i-1][k] == -1: #이전 상태가 불가능한 경우는 패스
