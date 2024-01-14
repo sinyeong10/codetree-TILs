7f21f1a18488eb20c930bd2fe44c585742d0b99a
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-14 19:31:47 +0900

    Title: 최대 증가 부분 수열, Time: 187ms, Memory: 25MB, Status: Passed - CodeTree

--- 240114/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 240114/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -4,13 +4,14 @@ base = list(map(int, stdin.readline().split()))
 
 dp = [0 for _ in range(n)] #i는 i번째까지 본 것
 
-dp[0] = 1
+#처음만 초기화하는 것이 아니라 이후에 처음일 수 있으므로 1부터 max_value가 시작해야 됨
+# dp[0] = 1
 
-for i in range(1, n):
-    max_value = 0
+for i in range(n):
+    max_value = 1
     for j in range(i): #이전에서 가능한 값
         if base[j] < base[i]:
             max_value = max(max_value, dp[j]+1)
     dp[i] = max_value
-    
+
 print(max(dp))
