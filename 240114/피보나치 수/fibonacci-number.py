20802b1c9f56f234dea4e2b89af46d8125280eea
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-14 18:38:58 +0900

    Title: 피보나치 수, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240114/피보나치 수/fibonacci-number.py
+++ 240114/피보나치 수/fibonacci-number.py
@@ -3,7 +3,12 @@ n = int(stdin.readline())
 
 dp = [0 for _ in range(n+1)]
 
-dp[1], dp[2] = 1, 1
+dp[1] = 1
+
+#2번째 값을 설정하지 않을 수 있음!
+if n > 1:
+    dp[2] = 1
+
 for i in range(3, n+1):
     dp[i] = dp[i-1]+dp[i-2]
 print(dp[n])
