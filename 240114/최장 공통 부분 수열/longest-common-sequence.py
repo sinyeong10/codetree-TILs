020976e936c93ec030663f46dfc1aa65f0cb61d9
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-14 18:25:15 +0900

    Title: 최장 공통 부분 수열, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240114/최장 공통 부분 수열/longest-common-sequence.py
@@ -0,0 +1,29 @@
+from sys import stdin
+a = stdin.readline().strip()
+b = stdin.readline().strip()
+
+dp = [[0]*len(b) for _ in range(len(a))] #dp[i][j]는 a의 i번째 문자, b의 j번째 문자까지의 최장 공통 수열
+
+for i in range(len(b)):
+    if a[0] == b[i]:
+        dp[0][i] = 1
+
+for j in range(len(a)):
+    if a[j] == b[0]:
+        dp[j][0] = 1
+
+# for elem in dp:
+#     print(*elem)
+
+for i in range(1, len(a)):
+    for j in range(1, len(b)):
+        if a[i] == b[j]: #같으면 추가됨
+            dp[i][j] = dp[i-1][j-1] + 1
+        else: #다르면 선택 안하고 이전의 최선을 가져옴
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+
+# for elem in dp:
+#     print(*elem)
+
+print(dp[len(a)-1][len(b)-1])
