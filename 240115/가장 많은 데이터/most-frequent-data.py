58d6930e590d751e715d181dcc3530f10d816a57
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-15 18:29:40 +0900

    Title: 가장 많은 데이터, Time: 286ms, Memory: 31MB, Status: Passed - CodeTree

--- 240115/가장 많은 데이터/most-frequent-data.py
+++ 240115/가장 많은 데이터/most-frequent-data.py
@@ -2,14 +2,17 @@ from sys import stdin
 n = int(stdin.readline())
 base = {}
 
+max_value = 0
+
 for i in range(n):
     sentence = stdin.readline().strip()
     if sentence in base:
         base[sentence] += 1
     else:
         base[sentence] = 1
+    max_value = max(max_value , base[sentence])
+#for문으로 순회하는 대신 넣을 때 처리하는 것도 가능!
+# for i in base.values():
+#     max_value = max(max_value , i)
 
-max_value = 0
-for i in base.values():
-    max_value = max(max_value , i)
 print(max_value)
