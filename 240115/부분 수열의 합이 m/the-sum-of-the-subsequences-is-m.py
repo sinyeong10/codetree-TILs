10a82a42a6834e12b3336c7c73571839b8ef9744
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-15 18:10:42 +0900

    Title: 부분 수열의 합이 m, Time: 179ms, Memory: 25MB, Status: Passed - CodeTree

--- 240115/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.py
+++ 240115/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.py
@@ -14,4 +14,8 @@ for j in range(n): #해당 원소를 한번씩만 체크해야함
         if base[j] > i or dp[i-base[j]] == stand_num: #동전 가치가 금액보다 큰 경우 패스, 해당 동전 선택 전의 값이 불가능한 경우 패스
             continue
         dp[i] = min(dp[i], dp[i-base[j]]+1)
+
+        # if i >= A[j]: #i가 동전의 가치보다 큰경우만 체크
+        #     dp[i] = min(dp[i], dp[i-A[j]]+1) #이전이 stand_num시 dp[i]의 값이 그대로 됨
+
 print(-1 if dp[m] == stand_num else dp[m])
