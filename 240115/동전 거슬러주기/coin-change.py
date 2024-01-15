ac45c9714510e2400ddee33738640a88404e1fd2
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-15 17:40:20 +0900

    Title: 동전 거슬러주기, Time: 128ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240115/동전 거슬러주기/coin-change.py
+++ 240115/동전 거슬러주기/coin-change.py
@@ -11,5 +11,5 @@ for i in range(1, m+1): #1~m원까지 필요한 동전의 갯수
         if i < base[j]: #금액보다 동전 가치가 크면 패스
             continue
         min_value = min(min_value, dp[i-base[j]]+1) #현재 선택한 동전의 가치 전의 합과 현재 동전을 선택
-    dp[i] = min_value
+    dp[i] = min_value if min_value != float("inf") else 0 #동전을 선택 불가능한 금액이 있을 수 있음!
 print(dp[m])
