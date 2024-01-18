bd1487fbaa66818e9481ba08dd97ea7eb60a8d6f
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-18 23:29:24 +0900

    Title: 큰 숫자만 계속 고르기, Time: 214ms, Memory: 31MB, Status: Passed - CodeTree

--- 240118/큰 숫자만 계속 고르기/keep-picking-the-big-number.py
+++ 240118/큰 숫자만 계속 고르기/keep-picking-the-big-number.py
@@ -8,6 +8,7 @@ for i in range(n):
     heapq.heappush(max_heap, -base[i])
 
 for _ in range(m):
-    heapq.heappush(max_heap, -(-heapq.heappop(max_heap)-1))
+    max_num = -heapq.heappop(max_heap)-1 #최댓값에서 1뺌
+    heapq.heappush(max_heap, -max_num) #다시 넣음
 
 print(-max_heap[0])
