a631b3507957c8e302ae5ed8316532bb936e1d63
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-18 23:23:32 +0900

    Title: 정수 명령 처리 6, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240118/정수 명령 처리 6/process-numeric-commands-6.py
@@ -0,0 +1,45 @@
+import heapq
+
+class PriorityQueue:
+    def __init__(self):          # 빈 Priority Queue 하나를 생성합니다.
+        self.items = []
+                
+    def push(self, item):        # 우선순위 큐에 데이터를 추가합니다.
+        heapq.heappush(self.items, -item)
+                
+    def empty(self):             # 우선순위 큐가 비어있으면 True를 반환합니다.
+        return not self.items
+                
+    def size(self):              # 우선순위 큐에 있는 데이터 수를 반환합니다.
+        return len(self.items)
+        
+    def pop(self):               # 우선순위 큐에 있는 데이터 중 최댓값에 해당하는 데이터를 반환하고 제거합니다.
+        if self.empty():
+            raise Exception("PriorityQueue is empty")
+            
+        return -heapq.heappop(self.items)
+                
+    def top(self):               # 우선순위 큐에 있는 데이터 중 최댓값에 해당하는 데이터를 제거하지 않고 반환합니다.
+        if self.empty():
+            raise Exception("PriorityQueue is empty")
+                        
+        return -self.items[0]
+
+from sys import stdin
+n = int(stdin.readline())
+base = PriorityQueue()
+for _ in range(n):
+    order = stdin.readline().split()
+    if order[0] == "push":
+        base.push(int(order[1]))
+    elif order[0] == "pop":
+        print(base.pop())
+    elif order[0] == "size":
+        print(base.size())
+    elif order[0] == "empty":
+        if base.empty():
+            print(1)
+        else:
+            print(0)
+    elif order[0] == "top":
+        base.top()
