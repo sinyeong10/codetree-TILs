1d93434285c2c39ec64626cf4faeba508d3a2036
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-19 16:55:24 +0900

    Title: 최대 숫자 구하기, Time: 516ms, Memory: 40MB, Status: Passed - CodeTree

--- 240119/최대 숫자 구하기/find-maximum-number.py
+++ 240119/최대 숫자 구하기/find-maximum-number.py
@@ -8,4 +8,4 @@ base = SortedSet(list(range(1, m+1)))
 for i in range(n):
     if numbers[i] in base:
         base.remove(numbers[i])
-    print(max(base))
+    print(base[-1]) #정렬되어있어 max대신 index로 가능!
