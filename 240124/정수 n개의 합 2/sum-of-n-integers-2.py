e5140b717aea518c6e0150640bdde45e852ad98d
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-24 22:55:57 +0900

    Title: 정수 n개의 합 2, Time: 204ms, Memory: 31MB, Status: Passed - CodeTree

--- 240124/정수 n개의 합 2/sum-of-n-integers-2.py
+++ 240124/정수 n개의 합 2/sum-of-n-integers-2.py
@@ -10,6 +10,6 @@ for i in range(1, n+1):
 # print(prefix_sum)
 
 max_value = 0
-for i in range(1, n-k+1):
+for i in range(1, n-k+1): #n-k의 값에 +k-1의 연산이 들어간 인덱스가 n-1이면 된다.
     max_value = max(max_value, prefix_sum[i+k-1]-prefix_sum[i-1])
 print(max_value)
