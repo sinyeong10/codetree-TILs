00cedf384cde87e610595b86dc74447465b626c2
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-24 22:54:09 +0900

    Title: 정수 n개의 합 2, Time: 137ms, Memory: 31MB, Status: Passed - CodeTree

---
+++ 240124/정수 n개의 합 2/sum-of-n-integers-2.py
@@ -0,0 +1,15 @@
+from sys import stdin
+n, k = list(map(int, stdin.readline().split()))
+base = list(map(int, stdin.readline().split()))
+
+prefix_sum = [0]*(n+1)
+prefix_sum[0] = 0
+for i in range(1, n+1):
+    prefix_sum[i] = prefix_sum[i-1]+base[i-1]
+
+# print(prefix_sum)
+
+max_value = 0
+for i in range(1, n-k+1):
+    max_value = max(max_value, prefix_sum[i+k-1]-prefix_sum[i-1])
+print(max_value)
