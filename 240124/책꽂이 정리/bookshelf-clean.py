41ed980c9c6cf14ea99c06bc0e6cc716185d9d23
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-24 22:38:28 +0900

    Title: 책꽂이 정리, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240124/책꽂이 정리/bookshelf-clean.py
+++ 240124/책꽂이 정리/bookshelf-clean.py
@@ -47,59 +47,83 @@ for i in range(1, n+1):
 # print()
 
 for _ in range(q):
-    order = list(map(int, stdin.readline().split()))
+    order = list(map(int, stdin.readline().split())) #다 숫자라 숫자로 처리했음!
     i, j = order[1], order[2]
     if order[0] == 1:
-        if len(bookcase_idx[j])==0:
+        if bookcase_idx[i].head is None: #변경할 값이 없으면 패스
+            continue
+
+        if len(bookcase_idx[j])==0: #들어갈 곳에 아무것도 없는 상태
             bookcase_idx[j].head = bookcase_idx[i].head
             bookcase_idx[j].tail = bookcase_idx[i].head
         else:
             connect(bookcase_idx[j].tail, bookcase_idx[i].head)
-        
-        if bookcase_idx[i].head is not None:
-            bookcase_idx[i].head = bookcase_idx[i].head.next
             bookcase_idx[j].tail = bookcase_idx[j].tail.next
-            disconnect(bookcase_idx[j].tail, bookcase_idx[i].head)
-            bookcase_idx[j].nodelen += 1
-            bookcase_idx[i].nodelen -= 1
+
+        bookcase_idx[i].head = bookcase_idx[i].head.next
+        
+        # print(bookcase_idx[i].head.data, bookcase_idx[i].tail.data)
+        # print(bookcase_idx[j].head.data, bookcase_idx[j].tail.data)
+        disconnect(bookcase_idx[j].tail, bookcase_idx[i].head)
+        bookcase_idx[j].nodelen += 1
+        bookcase_idx[i].nodelen -= 1
     elif order[0] == 2:
-        if len(bookcase_idx[j])==0:
+        if bookcase_idx[i].tail is None: #변경할 값이 없으면 패스
+            continue
+
+        if len(bookcase_idx[j])==0: #들어갈 곳에 아무것도 없는 상태
             bookcase_idx[j].head = bookcase_idx[i].tail
             bookcase_idx[j].tail = bookcase_idx[i].tail
         else:
             connect(bookcase_idx[i].tail, bookcase_idx[j].head)
-
-        if bookcase_idx[i].tail is not None:
-            bookcase_idx[i].tail = bookcase_idx[i].tail.prev
             bookcase_idx[j].head = bookcase_idx[j].head.prev
-            disconnect(bookcase_idx[i].tail, bookcase_idx[j].head)
-            bookcase_idx[j].nodelen += 1
-            bookcase_idx[i].nodelen -= 1
+
+        bookcase_idx[i].tail = bookcase_idx[i].tail.prev
+
+        disconnect(bookcase_idx[i].tail, bookcase_idx[j].head)
+        bookcase_idx[j].nodelen += 1
+        bookcase_idx[i].nodelen -= 1
     elif order[0] == 3:
-        if len(bookcase_idx[j])==0:
-            bookcase_idx[j].head = bookcase_idx[i].tail
+        if bookcase_idx[i].head is None: #변경할 값이 없으면 패스
+            continue
+
+        if len(bookcase_idx[j])==0: #들어갈 곳에 아무것도 없는 상태
+            bookcase_idx[j].head = bookcase_idx[i].head
             bookcase_idx[j].tail = bookcase_idx[i].tail
         else:
             connect(bookcase_idx[i].tail, bookcase_idx[j].head)
 
-        if bookcase_idx[i].tail is not None:
-            bookcase_idx[j].nodelen += len(bookcase_idx[i])
-            bookcase_idx[i].nodelen = 0
-            bookcase_idx[i].tail = bookcase_idx[i].tail.prev
-            bookcase_idx[j].head = bookcase_idx[j].head.prev
-        
+        bookcase_idx[j].head = bookcase_idx[i].head
+        if i != j:
+            bookcase_idx[i].head = None
+            bookcase_idx[i].tail = None
+        else:
+            disconnect(bookcase_idx[i].tail, bookcase_idx[i].head)
+
+        len_value = len(bookcase_idx[i]) #i,j가 같을 수 있음
+        bookcase_idx[j].nodelen += len_value
+        bookcase_idx[i].nodelen -= len_value
+
     elif order[0] == 4:
-        if len(bookcase_idx[j])==0:
+        if bookcase_idx[i].head is None: #변경할 값이 없으면 패스
+            continue
+
+        if len(bookcase_idx[j])==0: #들어갈 곳에 아무것도 없는 상태
             bookcase_idx[j].head = bookcase_idx[i].head
-            bookcase_idx[j].tail = bookcase_idx[i].head
+            bookcase_idx[j].tail = bookcase_idx[i].tail
         else:
             connect(bookcase_idx[j].tail, bookcase_idx[i].head)
 
-        if bookcase_idx[i].head is not None:
-            bookcase_idx[j].nodelen += len(bookcase_idx[i])
-            bookcase_idx[i].nodelen = 0
-            bookcase_idx[i].head = bookcase_idx[i].head.next
-            bookcase_idx[j].tail = bookcase_idx[j].tail.next
+        bookcase_idx[j].tail = bookcase_idx[i].tail
+        if i != j:
+            bookcase_idx[i].head = None
+            bookcase_idx[i].tail = None
+        else:
+            disconnect(bookcase_idx[i].tail, bookcase_idx[i].head)
+
+        len_value = len(bookcase_idx[i]) #i,j가 같을 수 있음
+        bookcase_idx[j].nodelen += len_value
+        bookcase_idx[i].nodelen -= len_value
 
 for i in range(1, k+1):
     print(len(bookcase_idx[i]), end=" ")
