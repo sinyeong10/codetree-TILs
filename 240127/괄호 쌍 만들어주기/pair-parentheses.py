897ffcb38285b2c70f04d69daf433e6fa417631a
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-27 21:20:01 +0900

    Title: 괄호 쌍 만들어주기, Time: 130ms, Memory: 26MB, Status: Passed - CodeTree

--- 240127/괄호 쌍 만들어주기/pair-parentheses.py
+++ 240127/괄호 쌍 만들어주기/pair-parentheses.py
@@ -3,12 +3,12 @@ sentence = stdin.readline().strip()
 
 count = 0 if sentence[-1] == "(" else 1
 R = [0 for _ in range(len(sentence))]
-for i in range(len(sentence)-2,-1,-1): #맨 마지막은 따로 처리
+for i in range(len(sentence)-2,-1,-1): #맨 마지막은 무조건 0이고 count에만 영향을 미쳐 따로 처리
     if sentence[i] == ")":
         count += 1
         if count == 2:
             R[i] += R[i+1]+1
-            count -= 1
+            count -= 1 #다음도 연속되는 지 체크를 위함
         else:
             R[i] = R[i+1]
     else:
@@ -24,7 +24,9 @@ for i in range(len(sentence)):
         count += 1
         if count == 2:
             answer += R[i]
-            count -= 1
+            count -= 1 #다음도 연속되는 지 체크를 위함
     else:
         count = 0
 print(answer)
+
+#count 체크보다 [i]와 [i+1]을 비교하는 게 더 간단하다..
