6f294c9eb16c546c59ca81d223bc32964207c9d9
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-29 22:39:22 +0900

    Title: 겹치는 숫자가 없는 최대 구간, Time: 198ms, Memory: 35MB, Status: Passed - CodeTree

--- 240129/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.py
+++ 240129/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.py
@@ -1,13 +1,13 @@
 from sys import stdin
 n = int(stdin.readline())
 base = list(map(int, stdin.readline().split()))
-count_set = set()
+count_set = set() #3개 이상이 제한일 경우에는 가능한 모든 숫자를 배열로 만들거나 Dict로 처리해야한다.
 
 ans = -1
 total = 0
 j = -1 #현재 위치로 다음 추가는 j+1로 이뤄짐!
 for i in range(n):
-    while j+1 < n and base[j+1] not in count_set:
+    while j+1 < n and base[j+1] not in count_set: #다음이 끝이 아니고 이전에 없어서 포함해도 되는 경우
         total += 1
         count_set.add(base[j+1])
         j+=1
