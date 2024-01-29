9033693151985c9ea308605a83b810d727f54155
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-29 21:13:45 +0900

    Title: 가장 짧은 부분합, Time: 212ms, Memory: 32MB, Status: Passed - CodeTree

--- 240129/가장 짧은 부분합/shortest-subtotal.py
+++ 240129/가장 짧은 부분합/shortest-subtotal.py
@@ -9,8 +9,13 @@ for i in range(n): #처음위치, 조건 충족시 계속 이동
     while j<n-1 and total < s: #현재 끝이 아니라면 total이 s넘을때까지 계속 더함
         total += base[j]
         j += 1
+
+    if total < s: #끝까지 이동했지만 여전히 s보다 작다면 탈출할 수 있음!!
+        break
+
     if total >= s: #조건 충족시 계산
         answer = min(answer, j-1-i+1) #j는 다음 j를 가리키고 있음!!
         # print(i, j, total, j-1-i+1)
+    
     total -= base[i]
 print(-1 if answer == n+1 else answer)
