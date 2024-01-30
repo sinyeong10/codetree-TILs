ae30c342e9ec64fdbfdb989206e848ad5b078abd
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-30 15:23:42 +0900

    Title: 부분수열 여부 판단하기, Time: 162ms, Memory: 33MB, Status: Passed - CodeTree

--- 240130/부분수열 여부 판단하기/determine-subsequence.py
+++ 240130/부분수열 여부 판단하기/determine-subsequence.py
@@ -6,10 +6,10 @@ B = list(map(int, stdin.readline().split()))
 def solve():
     i = -1 #A의 현재 위치, 이후의 값은 i+1로 추가됨
     for j in range(m): #m까지 조건 충족시 이동
-        while i < n and A[i+1]!=B[j]: #i가 n-1이하로 끝이 아니고 현재 매칭되지 않음
+        while i+1 < n and A[i+1]!=B[j]: #i가 n-1이하로 끝이 아니고 현재 매칭되지 않음
             i+=1 #따라서 한방향으로 이동하며 탐색
         
-        if i == n: #j와 매칭되는 게 없음
+        if i+1 == n: #j와 매칭되는 게 없음
             return False
         else: #찾아서 i,j 모두 이동
             i+=1
