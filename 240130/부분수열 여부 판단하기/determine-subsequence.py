1c82e041d4b78887c02532902ba90ad99b725bfe
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-30 15:25:49 +0900

    Title: 부분수열 여부 판단하기, Time: 205ms, Memory: 33MB, Status: Passed - CodeTree

--- 240130/부분수열 여부 판단하기/determine-subsequence.py
+++ 240130/부분수열 여부 판단하기/determine-subsequence.py
@@ -9,9 +9,10 @@ def solve():
         while i+1 < n and A[i+1]!=B[j]: #i가 n-1이하로 끝이 아니고 현재 매칭되지 않음
             i+=1 #따라서 한방향으로 이동하며 탐색
         
-        if i+1 == n: #j와 매칭되는 게 없음
+        if i+1 == n: #j와 매칭되는 게 없음, 매칭이 된다면 n-2로 마지막과 매칭되어야 함
             return False
         else: #찾아서 i,j 모두 이동
+            # print(i+1,j)
             i+=1
     return True
 
