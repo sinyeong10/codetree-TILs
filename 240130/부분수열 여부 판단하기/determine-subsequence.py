f612a7ff0db65fa3f78143feace93e9e35c41a64
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-01-30 15:29:27 +0900

    Title: 부분수열 여부 판단하기, Time: 256ms, Memory: 35MB, Status: Passed - CodeTree

--- 240130/부분수열 여부 판단하기/determine-subsequence.py
+++ 240130/부분수열 여부 판단하기/determine-subsequence.py
@@ -3,13 +3,27 @@ n, m = list(map(int, stdin.readline().split()))
 A = list(map(int, stdin.readline().split()))
 B = list(map(int, stdin.readline().split()))
 
+# def solve():
+#     i = -1 #A의 현재 위치, 이후의 값은 i+1로 추가됨
+#     for j in range(m): #m까지 조건 충족시 이동
+#         while i+1 < n and A[i+1]!=B[j]: #i가 n-1이하로 끝이 아니고 현재 매칭되지 않음
+#             i+=1 #따라서 한방향으로 이동하며 탐색
+        
+#         if i+1 == n: #j와 매칭되는 게 없음, 마지막에 매칭이 된다면 n-2로 m-1과 매칭되어야 함
+#             return False
+#         else: #찾아서 i,j 모두 이동
+#             # print(i+1,j)
+#             i+=1
+#     return True
+
+
 def solve():
-    i = -1 #A의 현재 위치, 이후의 값은 i+1로 추가됨
+    i = 0 #A의 다음 살펴볼 위치
     for j in range(m): #m까지 조건 충족시 이동
-        while i+1 < n and A[i+1]!=B[j]: #i가 n-1이하로 끝이 아니고 현재 매칭되지 않음
+        while i < n and A[i]!=B[j]: #i가 n-1이하로 끝이 아니고 현재 매칭되지 않음
             i+=1 #따라서 한방향으로 이동하며 탐색
         
-        if i+1 == n: #j와 매칭되는 게 없음, 마지막에 매칭이 된다면 n-2로 m-1과 매칭되어야 함
+        if i == n: #j와 매칭되는 게 없음, 마지막에 매칭이 된다면 n-2로 m-1과 매칭되어야 함
             return False
         else: #찾아서 i,j 모두 이동
             # print(i+1,j)
