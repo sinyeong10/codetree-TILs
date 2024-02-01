ecbdef5dd0d82a811b75229eb03b31cd5fd0a9ab
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-01 11:47:35 +0900

    Title: 정수 분배하기, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 240201/정수 분배하기/distributing-integers.py
+++ 240201/정수 분배하기/distributing-integers.py
@@ -6,14 +6,14 @@ def divide(number):
     total = 0
     for i in range(n):
         total += base[i]//number
-    return total
+    return total #여기서 조건 비교하고 아래에서 함수만 적어도 된다! 가능여부로 표현!
 
 left = 1
 right = 100000 #조건에서 가능한 가장 큰 정수
 max_value = 0
 while left<=right:
     mid = (left+right)//2
-    if divide(mid)>=m: #최댓값을 찾아 우측을 더 봄!
+    if divide(mid)>=m: #최댓값을 찾아 우측을 더 봄! #여기에 함수만 적어도 된다! 가능여부로 표현!
         max_value = max(max_value, mid)
         left = mid+1
     else:
