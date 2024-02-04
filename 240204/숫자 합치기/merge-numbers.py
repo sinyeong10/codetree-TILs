a963baec3b2ffeb1ff9eb0d1261899c81b6ae223
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-04 21:10:08 +0900

    Title: 숫자 합치기, Time: 312ms, Memory: 32MB, Status: Passed - CodeTree

--- 240204/숫자 합치기/merge-numbers.py
+++ 240204/숫자 합치기/merge-numbers.py
@@ -1,21 +1,38 @@
+#최솟값 2개는 뽑아서 활용하고 사라지고, 더했을 때 자동으로 정렬되어야 함
+
 from sys import stdin
 n = int(stdin.readline())
 
-from sortedcontainers import SortedSet
-base = SortedSet()
-count = 0
-for elem in list(map(int, stdin.readline().split())):
-    base.add((elem, count))
-    count += 1
+# #SortedSet이용
+# from sortedcontainers import SortedSet
+# base = SortedSet()
+# count = 0
+# for elem in list(map(int, stdin.readline().split())):
+#     base.add((elem, count))
+#     count += 1
+# total = 0
+# # print(base)
+# while len(base) >= 2: #2개 이상이면 계속 합침
+#     a,b = base[0], base[1]
+#     total += a[0]+b[0]
+#     base.remove(a)
+#     base.remove(b)
+#     base.add((a[0]+b[0], count))
+#     count += 1
+# print(total)
 
+#min-heap 이용
+import heapq
+base = list(map(int, stdin.readline().split()))
+pq = []
 total = 0
-# print(base)
+for elem in base:
+    heapq.heappush(pq, elem)
 
-while len(base) >= 2:
-    a,b = base[0], base[1]
-    total += a[0]+b[0]
-    base.remove(a)
-    base.remove(b)
-    base.add((a[0]+b[0], count))
-    count += 1
+while len(pq) >= 2: #2개 이상이면 계속 합침
+    a = heapq.heappop(pq)
+    b = heapq.heappop(pq)
+
+    total += a+b
+    heapq.heappush(pq, a+b)
 print(total)
