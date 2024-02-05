20be54d8c260209dbb902d4c571e4f5196aab6a7
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-05 10:26:07 +0900

    Title: 회의실 준비 구현, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240205/회의실 준비 구현/implement-scheduling-meeting-room.py
@@ -0,0 +1,15 @@
+from sys import stdin
+n = int(stdin.readline())
+base = [list(map(int, stdin.readline().split())) for _ in range(n)]
+base.sort(key = lambda x : (x[1], x[0]))
+print(base)
+
+total = 0
+last = 0
+for i in range(n):
+    if base[i][0] < last: #한 회의가 끝나기 전에 시작하는 회의임
+        continue
+    if base[i][1] >= last: #항상 만족, 끝나는 직후에 다른 회의가 불가능할 시 등호 사라짐
+        total += 1
+        last = base[i][1]
+print(total)
