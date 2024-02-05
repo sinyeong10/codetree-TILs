92fc69cb8f31a1265b9dcf2eee375519ebad265d
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-05 10:46:39 +0900

    Title: 회의실 준비 구현, Time: 487ms, Memory: 32MB, Status: Passed - CodeTree

--- 240205/회의실 준비 구현/implement-scheduling-meeting-room.py
+++ 240205/회의실 준비 구현/implement-scheduling-meeting-room.py
@@ -9,7 +9,8 @@ last = 0
 for i in range(n):
     if base[i][0] < last: #한 회의가 끝나기 전에 시작하는 회의임
         continue
-    if base[i][1] >= last: #항상 만족, 끝나는 직후에 다른 회의가 불가능할 시 등호 사라짐
-        total += 1
-        last = base[i][1]
+    #문제의 조건에 따라 마지막과 앞만 비교하면 됨
+    # if base[i][1] >= last: #항상 만족, 끝나는 직후에 다른 회의가 불가능할 시 등호 사라짐
+    total += 1
+    last = base[i][1]
 print(total)
