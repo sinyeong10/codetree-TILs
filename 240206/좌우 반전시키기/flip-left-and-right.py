e34fa13452e29f1ace6a0578c9cf5dd1b6dfbca1
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-06 16:55:51 +0900

    Title: 좌우 반전시키기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240206/좌우 반전시키기/flip-left-and-right.py
+++ 240206/좌우 반전시키기/flip-left-and-right.py
@@ -7,8 +7,18 @@ total = 0
 for i in range(1, n-1):
     if base[i-1] != 1: #앞이 1이 아니면 현재칸 주변 반전!
         total += 1
+        #명시적 표시
+        base[i-1] = trans[base[i-1]]
         base[i] = trans[base[i]]
         base[i+1] = trans[base[i+1]]
+    # print(base)
+
+#마지막 누를 경우 예외처리
+if base[-2] == 0:
+    total += 1
+    base[-2] = trans[base[-2]]
+    base[-1] = trans[base[-1]]
+# print(base)
 
 #마지막이 1이면 가능, 아니면 불가능
 if base[-1] == 1:
