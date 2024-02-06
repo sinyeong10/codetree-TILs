295bd4290747bf7deb04480be8358d95d1fcb2cf
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-06 15:07:10 +0900

    Title: G & H 반전시키기, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240206/G & H 반전시키기/reversing-g-and-h.py
+++ 240206/G & H 반전시키기/reversing-g-and-h.py
@@ -2,18 +2,19 @@ from sys import stdin
 n = int(stdin.readline())
 base = stdin.readline().strip()
 target = stdin.readline().strip()
-trans = [0 for _ in range(n)]
 
+#바꿔야할 위치
+trans = [0 for _ in range(n)]
 for i in range(n):
     if base[i] != target[i]:
         trans[i] += 1
+print(trans)
 
-# print(trans)
-
+#연속된 1로 이뤄진 서로 다른 그룹의 수!
 total = 0
 idx = 0
 for i in range(n):
-    if trans[i] == 1 and trans[i] != idx:
+    if trans[i] == 1 and trans[i] != idx: #처음 1일 때 갯수 증가!
         total += 1
     idx = trans[i]
 print(total)
