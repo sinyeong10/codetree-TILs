938e28c04dc0f83de1430d601c4074f735c1326d
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-06 17:36:38 +0900

    Title: 좌우 반전시키기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240206/좌우 반전시키기/flip-left-and-right.py
+++ 240206/좌우 반전시키기/flip-left-and-right.py
@@ -2,26 +2,32 @@ from sys import stdin
 n = int(stdin.readline())
 base = list(map(int, stdin.readline().split()))
 
-trans = {0 : 1, 1 : 0}
-total = 0
-for i in range(1, n-1):
-    if base[i-1] != 1: #앞이 1이 아니면 현재칸 주변 반전!
+def solve():
+    if n == 1:
+        print(-1)
+        return
+    trans = {0 : 1, 1 : 0}
+    total = 0
+    for i in range(1, n-1):
+        if base[i-1] != 1: #앞이 1이 아니면 현재칸 주변 반전!
+            total += 1
+            #명시적 표시
+            base[i-1] = trans[base[i-1]]
+            base[i] = trans[base[i]]
+            base[i+1] = trans[base[i+1]]
+        # print(base)
+
+    #마지막 누를 경우 예외처리
+    if base[-2] == 0:
         total += 1
-        #명시적 표시
-        base[i-1] = trans[base[i-1]]
-        base[i] = trans[base[i]]
-        base[i+1] = trans[base[i+1]]
+        base[-2] = trans[base[-2]]
+        base[-1] = trans[base[-1]]
     # print(base)
 
-#마지막 누를 경우 예외처리
-if base[-2] == 0:
-    total += 1
-    base[-2] = trans[base[-2]]
-    base[-1] = trans[base[-1]]
-# print(base)
+    #마지막이 1이면 가능, 아니면 불가능
+    if base[-1] == 1:
+        print(total)
+    else:
+        print(-1)
 
-#마지막이 1이면 가능, 아니면 불가능
-if base[-1] == 1:
-    print(total)
-else:
-    print(-1)
+solve()
