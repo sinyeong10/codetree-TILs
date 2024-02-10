59484af9829eb9e8b3c4a1dba04da008a5c601e3
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-10 12:50:06 +0900

    Title: 사각형 채우기 3, Time: 154ms, Memory: 24MB, Status: Passed - CodeTree

--- 240210/사각형 채우기 3/rectangle-fill-3.py
+++ 240210/사각형 채우기 3/rectangle-fill-3.py
@@ -7,7 +7,7 @@ dp[0] = 1
 dp[1] = 2
 
 #마지막에 mod연산을 하는 것보다 진행하면서 하는 것이 연산 상에 오버플로우를 막을 수 있다!
-
+#(a + b) % M = ((a % M) + (b % M)) % M
 for i in range(2, n+1):
     dp[i] = dp[i-1]*2 + dp[i-2]*3
     for j in range(2, i):
