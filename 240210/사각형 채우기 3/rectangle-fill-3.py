cd02b62a8dbb9368316649c11193515bb13eb803
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-10 12:43:52 +0900

    Title: 사각형 채우기 3, Time: 145ms, Memory: 25MB, Status: Passed - CodeTree

--- 240210/사각형 채우기 3/rectangle-fill-3.py
+++ 240210/사각형 채우기 3/rectangle-fill-3.py
@@ -8,7 +8,7 @@ dp[1] = 2
 
 for i in range(2, n+1):
     dp[i] = dp[i-1]*2 + dp[i-2]*3
-    if i > 2:
-        dp[i] += dp[i-3]*2
+    for j in range(2, i):
+        dp[i] += dp[i-(j+1)]*2
 
 print(dp[-1]%1000000007)
