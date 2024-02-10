ec6308278c6bcc846aec6a18e52d3b92de7bd009
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-10 12:35:48 +0900

    Title: 사각형 채우기 3, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240210/사각형 채우기 3/rectangle-fill-3.py
@@ -0,0 +1,12 @@
+from sys import stdin
+n = int(stdin.readline())
+
+dp = [0 for _ in range(n+1)]
+
+dp[0] = 1
+dp[1] = 2
+
+for i in range(2, n+1):
+    dp[i] = dp[i-1]*2 + dp[i-2]*3
+
+print(dp[-1]%1000000007)
