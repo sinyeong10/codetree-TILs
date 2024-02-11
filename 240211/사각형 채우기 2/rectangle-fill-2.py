5bd715952005aecd93fc0418bd9b54a899cbd16e
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-11 12:22:26 +0900

    Title: 사각형 채우기 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240211/사각형 채우기 2/rectangle-fill-2.py
@@ -0,0 +1,10 @@
+from sys import stdin
+n = int(stdin.readline())
+dp = [0 for _ in range(n+1)]
+
+dp[0] = 1 #의미적 초기화!
+dp[1] = 1
+
+for i in range(2,n+1):
+    dp[i] = dp[i-1]%10007+dp[i-2]*2%10007
+print(dp[-1])
