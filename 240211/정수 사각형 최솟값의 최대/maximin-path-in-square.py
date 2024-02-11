940e79254dbb33c920eb68c9029888717a54426b
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-11 15:51:38 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 153ms, Memory: 26MB, Status: Passed - CodeTree

--- 240211/정수 사각형 최솟값의 최대/maximin-path-in-square.py
+++ 240211/정수 사각형 최솟값의 최대/maximin-path-in-square.py
@@ -4,7 +4,7 @@ base_2d = [list(map(int, stdin.readline().split())) for _ in range(n)]
 
 dp_2d = [[0]*n for _ in range(n)]
 dp_2d[0][0] = base_2d[0][0]
-for j in range(1, n):
+for j in range(1, n): #우측으로 쭉 최소값나옴
     dp_2d[0][j] = min(dp_2d[0][j-1], base_2d[0][j])
 
 def in_range(x,y):
@@ -14,11 +14,11 @@ dx, dy = [-1, 0], [0, -1] #위, 좌측 체크
 
 for i in range(1, n):
     for j in range(n):
-        min_value = 0
+        min_value = 0 #이전 최솟값중 최대!
         for dxs, dys in zip(dx, dy):
             prev_i, prev_j = i+dxs, j+dys
             if in_range(prev_i, prev_j): #무조건 한번은 되고 범위 안이면 갱신 
                 min_value = max(min_value, dp_2d[prev_i][prev_j])
-        dp_2d[i][j] = min(base_2d[i][j], min_value)
+        dp_2d[i][j] = min(base_2d[i][j], min_value) #현재가 최소면 현재를 지나는 경로는 최소!
 
 print(dp_2d[-1][-1])
