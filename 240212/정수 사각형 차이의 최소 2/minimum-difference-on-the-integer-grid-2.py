4774fb876da22f9442e70307865bf1ae97e9acb7
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-12 17:55:28 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240212/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 240212/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -5,21 +5,25 @@ base_2d = [list(map(int, stdin.readline().split())) for _ in range(n)]
 def in_range(i,j):
     return 0<=i<n and 0<=j<n
 
-def stardard(num):
+def standard(num):
     def move(i, j, min_value, max_value):
-        # print(i, j, min_value)
+        # print(i, j, min_value, max_value)
         if i == n-1 and j == n-1:
-            return True
+            return max_value
             
+        tmp = 101
         dx, dy = [0,1],[1,0] #오른쪽, 아래 체크
         for dxs, dys in zip(dx, dy):
             next_i, next_j = i+dxs, j+dys
             if in_range(next_i, next_j):
-                tmp1, tmp2 = min(min_value, base_2d[next_i][next_j]), max(max_value, base_2d[next_i][next_j])
-                if abs(tmp2 - tmp1) <= num:
-                    if move(next_i, next_j, tmp1, tmp2):
-                        return True
-        return False
+                if base_2d[next_i][next_j] >= num:
+                    tmp1, tmp2 = min(min_value, base_2d[next_i][next_j]), max(max_value, base_2d[next_i][next_j])
+                    end_max = move(next_i, next_j, tmp1, tmp2)
+                    if end_max != 101:
+                        tmp = min(tmp, end_max)
+                # else:
+                    # print(i, j, next_i, next_j, base_2d[next_i][next_j])
+        return tmp
     return move(0,0,base_2d[0][0], base_2d[0][0])
 
 # min_value = 100
@@ -27,22 +31,79 @@ def stardard(num):
 # right = 100
 # while left<=right:
 #     mid = (left+right)//2
-#     if stardard(mid): #이정도 차이로 이동이 가능함!
+#     # print(mid)
+#     if standard(mid) != -1: #값의 최소가 이 값일 때 이동이 가능함!
+#         min_value = min(min_value, mid)
+#         left = mid+1
+#     else:
+#         right = mid-1
+# print(min_value)
+
+# print(standard(1))
+# print(standard(2))
+
+min_value = 100
+for i in range(1, 100):
+    tmp = standard(i)
+    print(i, tmp)
+    if tmp != 101:
+        min_value = min(min_value, tmp-i)
+    else:
+        break
+print(min_value)
+
+
+
+
+#이진탐색 : 시간초과
+# def in_range(i,j):
+#     return 0<=i<n and 0<=j<n
+
+# def standard(num):
+#     def move(i, j, min_value, max_value):
+#         # print(i, j, min_value)
+#         if i == n-1 and j == n-1:
+#             return True
+            
+#         dx, dy = [0,1],[1,0] #오른쪽, 아래 체크
+#         for dxs, dys in zip(dx, dy):
+#             next_i, next_j = i+dxs, j+dys
+#             if in_range(next_i, next_j):
+#                 tmp1, tmp2 = min(min_value, base_2d[next_i][next_j]), max(max_value, base_2d[next_i][next_j])
+#                 if abs(tmp2 - tmp1) <= num:
+#                     if move(next_i, next_j, tmp1, tmp2):
+#                         return True
+#         return False
+#     return move(0,0,base_2d[0][0], base_2d[0][0])
+
+# min_value = 100
+# left = 1
+# right = 100
+# while left<=right:
+#     mid = (left+right)//2
+#     if standard(mid): #이정도 차이로 이동이 가능함!
 #         min_value = min(min_value, mid)
 #         right = mid-1
 #     else:
 #         left = mid+1
 # print(min_value)
 
-# print(stardard(19))
-# print(stardard(20))
-# print(stardard(21))
+# print(standard(19))
+# print(standard(20))
+# print(standard(21))
 
-for i in range(1, 100):
-    if stardard(i):
-        print(i)
-        break
 
+
+
+
+
+
+
+
+#2차원 (최소, 최대) DP 반례가 존재
+# 2 4 2 
+# 2 1 2 
+# 2 1 4
 # dp_2d = [[(100, 1)]*n for _ in range(n)] #최소 최대로 갱신!
 
 # #항상 지나가는 점!
@@ -77,8 +138,8 @@ for i in range(1, 100):
 #                 dp_2d[i][j] = min_value, max_value
 
 #         # print(i,j)
-# for elem in dp_2d:
-#     print(*elem)
+# # for elem in dp_2d:
+# #     print(*elem)
 
 # a,b = dp_2d[-1][-1]
 # print(b-a)
