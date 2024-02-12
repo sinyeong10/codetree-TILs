38c94af0020e156404334df6c23ee8161483b081
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-12 21:15:39 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 407ms, Memory: 36MB, Status: Passed - CodeTree

--- 240212/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240212/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -10,23 +10,22 @@ def in_range(i,j):
     return 0<=i<n and 0<=j<n
 
 #마지막 루프에서 값이 가장 작아야하므로 total을 전달해서 증가시켜 호출하는 방법은 아님!
-def sol(i,j, total): #i,j에서 시작하는 경우의 갯수
+def sol(i,j): #i,j에서 시작하는 경우의 갯수
     # print("def :",i, j, total)
     if dp_2d[i][j] != 0: #이미 기록되어있으면 패스
         # print("이미 계산 :",i,j,dp_2d[i][j])
         return dp_2d[i][j]
 
     #더 갈 수 있는 곳이 있다면 가장 큰 경우를 가져옴
-    tmp = 0
+    tmp = 1 #현재 위치
     for dxs, dys in zip(dx, dy):
         next_i, next_j = i+dxs, j+dys
         if in_range(next_i, next_j): #다음이 범위 밖이 아니면!
             if base_2d[i][j] < base_2d[next_i][next_j]: #더 큰 값인 경우!
-                tmp = max(tmp, sol(next_i, next_j, total))
+                tmp = max(tmp, sol(next_i, next_j)+1) #이후의 최대와 현재위치
 
-    #현제 위치를 선택하고 반환함!
-    dp_2d[i][j] = tmp+1
-    return tmp+1
+    dp_2d[i][j] = tmp
+    return tmp
 
 
 # sol(2,0,0)
@@ -36,7 +35,7 @@ def sol(i,j, total): #i,j에서 시작하는 경우의 갯수
 max_value = 0
 for i in range(n):
     for j in range(n):
-        max_value = max(max_value, sol(i, j, 0))
+        max_value = max(max_value, sol(i, j))
 print(max_value)
 
 # for elem in dp_2d:
