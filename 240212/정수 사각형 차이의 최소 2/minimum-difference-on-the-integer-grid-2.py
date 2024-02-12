c2cce0876ee7c5bcb17fe4505bb81b980461c98a
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-12 13:37:18 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 141ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240212/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -0,0 +1,39 @@
+from sys import stdin
+n = int(stdin.readline())
+base_2d = [list(map(int, stdin.readline().split())) for _ in range(n)]
+
+dp_2d = [[(100, 1)]*n for _ in range(n)] #최소 최대로 갱신!
+
+dp_2d[0][0] = (base_2d[0][0], base_2d[0][0])
+for j in range(1,n):
+    min_value, max_value = dp_2d[0][j-1]
+    min_value = min(min_value, base_2d[0][j])
+    max_value = max(max_value, base_2d[0][j])
+    dp_2d[0][j] = (min_value, max_value)
+
+for i in range(1, n):
+    min_value, max_value = dp_2d[i-1][0]
+    min_value = min(min_value, base_2d[i][0])
+    max_value = max(max_value, base_2d[i][0])
+    dp_2d[i][0] = (min_value, max_value)
+
+dx, dy = [0,-1],[-1,0] #왼쪽, 위 체크
+
+for i in range(1, n):
+    for j in range(1, n):
+        standard = float("inf")
+        for dxs, dys in zip(dx, dy):
+            prev_i, prev_j = i+dxs, j+dys
+            min_value, max_value = dp_2d[prev_i][prev_j]
+            if standard > max_value-min_value: #차이가 더 작으면 갱신
+                standard = max_value - min_value #이걸로 기준이 바껴서 이전보다 작을 시 반영
+                min_value = min(min_value, base_2d[i][j])
+                max_value = max(max_value, base_2d[i][j])
+                dp_2d[i][j] = min_value, max_value
+
+        # print(i,j)
+        # for elem in dp_2d:
+        #     print(*elem)
+
+a,b = dp_2d[-1][-1]
+print(b-a)
