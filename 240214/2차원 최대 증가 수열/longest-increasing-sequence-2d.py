a15dcf0761427b0243b622dfdbc7b52db169fcca
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-14 00:51:25 +0900

    Title: 2차원 최대 증가 수열, Time: 140ms, Memory: 25MB, Status: Passed - CodeTree

--- 240214/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 240214/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -1,15 +1,16 @@
 from sys import stdin
-n, m = list(map(int, stdin.readline().split()))
+n, m = list(map(int, stdin.readline().split())) #nxn이 아니라 nxm이다 범위 잘 보자....
 base_2d = [list(map(int, stdin.readline().split())) for _ in range(n)]
 
-dp_2d = [[0]*n for _ in range(n)]
+dp_2d = [[0]*m for _ in range(n)]
 dp_2d[0][0] = 1
 
 for i in range(1, n):
-    for j in range(1, n):
+    for j in range(1, m):
 
         #이전 위치의 값을 가져옴
         if base_2d[i][j] <= base_2d[0][0]:
+            # print(i,j)
             continue
         else:
             dp_2d[i][j] = 2
@@ -23,6 +24,6 @@ for i in range(1, n):
 
 max_value = 0
 for i in range(n):
-    for j in range(n):
+    for j in range(m):
         max_value = max(max_value, dp_2d[i][j])
 print(max_value)
