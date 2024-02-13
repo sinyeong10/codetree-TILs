c893f37e4dd45689966d448262609d70161a28a1
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-14 00:03:25 +0900

    Title: 최대 점프 횟수, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240214/최대 점프 횟수/maximum-number-of-jumps.py
+++ 240214/최대 점프 횟수/maximum-number-of-jumps.py
@@ -5,8 +5,9 @@ dp = [0 for _ in range(n)]
 dp[0] = 0
 for i in range(1, n):
     check = False
-    for j in range(i): #이전 체크
-        if base[j] >= i-j: #현재위치에서 떨어진 것 이상의 값일 경우 갱신 시도! #근데 인덱스가 햇갈림....
+    for j in range(i):
+        #이전 체크 #base[j]+j >= i으로 j위치에서 점프해서 i를 넘으면 됨
+        if base[j] >= i-j: #현재위치에서 떨어진 것 이상의 값일 경우 갱신 시도!
             check = True
             dp[i] = max(dp[i], dp[j]+1)
     if not check: #이전이 다 불가능하면 멈춤!
