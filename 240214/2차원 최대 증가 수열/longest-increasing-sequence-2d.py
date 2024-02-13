238dfa33c07609b03dfe4b22417cbd07709ffa54
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-14 00:55:10 +0900

    Title: 2차원 최대 증가 수열, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

--- 240214/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 240214/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -7,18 +7,30 @@ dp_2d[0][0] = 1
 
 for i in range(1, n):
     for j in range(1, m):
+        #i,j의 이전, q,w를 보고 가능한지 봄
+        for q in range(i):
+            for w in range(j):
+                if dp_2d[q][w] == 0: #여기는 불가능!
+                    continue
 
-        #이전 위치의 값을 가져옴
-        if base_2d[i][j] <= base_2d[0][0]:
-            # print(i,j)
-            continue
-        else:
-            dp_2d[i][j] = 2
-
-        for q in range(1, i):
-            for w in range(1, j):
                 if base_2d[i][j] > base_2d[q][w]:
                     dp_2d[i][j] = max(dp_2d[i][j], dp_2d[q][w]+1)
+
+# for i in range(1, n):
+#     for j in range(1, m):
+
+#         #이전 위치의 값을 가져옴
+#         if base_2d[i][j] <= base_2d[0][0]:
+#             # print(i,j)
+#             continue
+#         else:
+#             dp_2d[i][j] = 2
+
+#         for q in range(1, i):
+#             for w in range(1, j):
+
+#                 if base_2d[i][j] > base_2d[q][w]:
+#                     dp_2d[i][j] = max(dp_2d[i][j], dp_2d[q][w]+1)
 
 # print(dp_2d)
 
