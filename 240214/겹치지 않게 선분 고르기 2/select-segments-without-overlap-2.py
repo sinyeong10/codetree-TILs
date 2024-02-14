2dfcaac54579bdc47552b46ac2fee85a6bd3f268
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-14 22:44:15 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 146ms, Memory: 25MB, Status: Passed - CodeTree

--- 240214/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
+++ 240214/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -3,13 +3,33 @@ n = int(stdin.readline())
 lines = []
 for _ in range(n):
     lines.append(list(map(int, stdin.readline().split())))
-lines.sort(key = lambda x : (x[1], x[0]))
+
+
+# #그리디
+# lines.sort(key = lambda x : (x[1], x[0])) #마지막 기준으로 정렬
+# # print(lines)
+
+# last = -1
+# total = 0
+# for x1, x2 in lines:
+#     if x1 > last:
+#         last = x2
+#         total += 1
+# print(total)
+
+#dp
+lines.sort() #처음 기준으로 정렬
 # print(lines)
 
-last = -1
-total = 0
-for x1, x2 in lines:
-    if x1 > last:
-        last = x2
-        total += 1
-print(total)
+dp = [0 for _ in range(n)]
+for i in range(n):
+    dp[i] = 1 #해당 선분 선택
+    for j in range(i): #이전의 선분 체크
+        x1_i, _ = lines[i]
+        _, x2_j = lines[j]
+
+        #처음 기준으로 정렬되어 있음! 끝이 작기만 하면 됨
+        if x2_j < x1_i:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))
