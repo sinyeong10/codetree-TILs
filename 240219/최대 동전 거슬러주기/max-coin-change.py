43afdb63603ae723ad4429ad8518253e27ae4ab7
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-19 00:21:25 +0900

    Title: 최대 동전 거슬러주기, Time: 159ms, Memory: 25MB, Status: Passed - CodeTree

--- 240219/최대 동전 거슬러주기/max-coin-change.py
+++ 240219/최대 동전 거슬러주기/max-coin-change.py
@@ -2,9 +2,10 @@ from sys import stdin
 n, m = list(map(int, stdin.readline().split()))
 coin = list(map(int, stdin.readline().split()))
 
-dp = [0]*(m+1)
+dp = [-1]*(m+1)
+dp[0] = 0
 for i in range(1, m+1):
     for j in coin:
-        if i >= j:
+        if i >= j and dp[i-j] != -1: #불가능한 경우를 제외
             dp[i] = max(dp[i], dp[i-j]+1)
 print(dp[-1])
