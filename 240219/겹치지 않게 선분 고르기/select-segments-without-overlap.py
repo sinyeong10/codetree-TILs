7d5dcc7d86bce2f3838bbb1426ec2a688e4e2c6b
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-19 15:06:56 +0900

    Title: 겹치지 않게 선분 고르기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240219/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -0,0 +1,19 @@
+from sys import stdin
+n = int(stdin.readline())
+line = [list(map(int, stdin.readline().split())) for _ in range(n)]
+line.sort() #앞이 빠른 순서대로 정렬, 이후 빨리 끝나는 순서대로 정렬
+
+def solve(idx, last): #idx까지 봄, 현재 last가 마지막
+    total = 0
+    if idx == n: #다 보면 선택x로 0을 반환
+        return total
+    x, y = line[idx] #현재 보는 선분
+    max_value = 0
+    if last < x: #이 경우는 idx를 선택해서 다음으로 가능!
+        total += 1
+        max_value = max(max_value, solve(idx+1, y))
+    #idx를 선택하지 않는 경우!
+    max_value = max(max_value, solve(idx+1, last))
+    return total + max_value
+
+print(solve(0, -1))
