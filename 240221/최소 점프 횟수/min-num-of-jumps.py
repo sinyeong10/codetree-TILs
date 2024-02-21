d8d8ac18fbdf890fb8338afba1b4912234df7aef
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-21 11:15:11 +0900

    Title: 최소 점프 횟수, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 240221/최소 점프 횟수/min-num-of-jumps.py
+++ 240221/최소 점프 횟수/min-num-of-jumps.py
@@ -2,15 +2,25 @@ from sys import stdin
 n = int(stdin.readline())
 base = list(map(int, stdin.readline().split()))
 
+# def sol(idx):
+#     min_value = float("inf")
+#     if idx == n-1:
+#         return 0
+
+#     # print(idx,base[idx]+1, n-idx)
+#     for i in range(1, min(base[idx]+1, n-idx)):
+#         min_value = min(min_value, sol(idx+i)+1)
+#     return min_value
+
 def sol(idx):
     min_value = float("inf")
-    if idx == n-1:
+    if idx >= n-1:
         return 0
 
     # print(idx,base[idx]+1, n-idx)
-    for i in range(1, min(base[idx]+1, n-idx)):
+    for i in range(1, base[idx]+1):
         min_value = min(min_value, sol(idx+i)+1)
     return min_value
-    
+
 tmp = sol(0)
 print(tmp if tmp != float("inf") else -1)
