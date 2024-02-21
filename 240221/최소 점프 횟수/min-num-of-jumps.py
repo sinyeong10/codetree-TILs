d0efa3a7fc05b8095574f895f491b4861dc7b940
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-21 11:16:30 +0900

    Title: 최소 점프 횟수, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240221/최소 점프 횟수/min-num-of-jumps.py
+++ 240221/최소 점프 횟수/min-num-of-jumps.py
@@ -14,6 +14,7 @@ base = list(map(int, stdin.readline().split()))
 
 def sol(idx):
     min_value = float("inf")
+    #넘으면 그 이전에서 가능하다는 이야기이므로 i의 범위를 인덱스 범위내로 제한하지 않아도 됨!
     if idx >= n-1:
         return 0
 
