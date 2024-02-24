651f5b3885838561f5da261ad4e8f74edd692081
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-24 20:35:15 +0900

    Title: 최소 차 분할, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240224/최소 차 분할/minimum-diff-partition.py
+++ 240224/최소 차 분할/minimum-diff-partition.py
@@ -7,11 +7,11 @@ total = sum(base)
 dp = [False]*(total//2+1) #0~total//2까지 봄, 작은 부분으로 i를 정의! total-i-i가 차이가 됨!
 dp[0] = True
 for elem in base:
-    for j in range(elem, total//2+1): #좌측 기준!, 항상 j는 elem 이상임!
-        if dp[j-elem]:
+    for j in range(total//2,elem-1,-1): #역순으로 해야 elem의 계산 중복이 없음!
+        if dp[j-elem]: #좌측 기준!, 항상 j는 elem 이상임! 
             dp[j] = True
 # print(dp)
-
+# print(total, total//2)
 for tmp in range(total//2, -1, -1): #total~0
     if dp[tmp]:
         print(total-tmp-tmp)
