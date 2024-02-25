677889d38c01688229331b132aedfcbbf89c6384
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-25 21:27:17 +0900

    Title: 최대 합 분할, Time: 2041ms, Memory: 137MB, Status: Passed - CodeTree

--- 240225/최대 합 분할/maximum-sum-partition.py
+++ 240225/최대 합 분할/maximum-sum-partition.py
@@ -41,26 +41,41 @@ total = sum(base)
 #         print(check)
 #         break
 
-#dp[i][j] : i번째 수까지 고려했을 때 그룹 A합 - 그룹 B합 (항상 그룹 A가 크게 함!)
-dp = [[-1]*(total+1) for _ in range(n+1)]
-dp[0][0] = 0
+#dp[i][j] : i번째 수까지 고려했을 때 그룹 A합 - 그룹 B합 (항상 그룹 A가 크게 하는 건 힘듦, -의 경우가 생략되 버림
+#OFFSET으로 -도 처리!
+OFFSET = total
+dp = [[float("-inf")]*(total*2+1) for _ in range(n+1)]
+dp[0][OFFSET] = 0
 # print(len(dp), len(dp[0]), len(dp[0][0])) #n+1*total+1*2
 
 for i in range(1,n+1):
     elem = base[i]
 
+    #모두 i-1을 기반으로 연산하여 중복은 발생하지 않음!
+
     #C로 인해 이전에 가능했으면 지금도 가능!
-    for j in range(total+1):
-        if dp[i-1][j] != -1:
-            dp[i][j] = dp[i-1][j]
+    for j in range(total*2+1):
+        if dp[i-1][j] != float("-inf"):
+            dp[i][j] = max(dp[i][j], dp[i-1][j])
 
-    for j in range(total+1):
+    for j in range(total*2+1):
         #좌측으로 체크, A에 추가되는 경우!
-        if j >= elem and dp[i-1][j-elem] != -1:
-            dp[i][j] = dp[i-1][j-elem]+elem
+        if j >= elem and dp[i-1][j-elem] != float("-inf"):
+            # print("왼", j-OFFSET, elem, dp[i-1][j-elem])
+            dp[i][j] = max(dp[i][j], dp[i-1][j-elem]+elem)
 
+    for j in range(total*2+1, -1, -1):
         #우측으로 체크, B에 추가되는 경우!
-        if j+elem <= total and dp[i-1][j+elem] != -1:
-            dp[i][j] = dp[i-1][j+elem]
+        if j+elem <= total*2 and dp[i-1][j+elem] != float("-inf"):
+            # print("오", j-OFFSET, elem, dp[i-1][j+elem])
+            dp[i][j] = max(dp[i][j], dp[i-1][j+elem])
 
-print(dp[-1][0])
+print(dp[-1][OFFSET])
+# print(OFFSET)
+# for elem in dp:
+#     print(base)
+#     print(elem[:OFFSET])
+#     print(elem[OFFSET])
+#     print(elem[OFFSET+1:])
+#     print()
+# print(dp[-1], OFFSET, dp[-1][OFFSET])
