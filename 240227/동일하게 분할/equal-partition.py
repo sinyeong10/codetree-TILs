ee0eb098e1c4193ac1d2c9d86e56e0600eeae81d
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-27 06:42:26 +0900

    Title: 동일하게 분할, Time: 111ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240227/동일하게 분할/equal-partition.py
+++ 240227/동일하게 분할/equal-partition.py
@@ -2,17 +2,54 @@ from sys import stdin
 n = int(stdin.readline())
 base = [0]+list(map(int, stdin.readline().split()))
 total = sum(base)
-dp = [[[False]*(total+1) for _ in range(total+1)] for _ in range(n+1)] #i까지 봐서 A의 합이 j, B의 합이 k
+
+
+#3차원 DP 메모리 초과
+# dp = [[[False]*(total+1) for _ in range(total+1)] for _ in range(n+1)] #i까지 봐서 A의 합이 j, B의 합이 k
+# # print(len(dp), len(dp[0]), len(dp[0][0]))
+# dp[0][0][0] = True
+
+# for i in range(1, n+1):
+#     for j in range(total+1):
+#         for k in range(total+1):
+#             if dp[i-1][j][k]:
+#                 dp[i][j][k] = True
+    
+#     elem = base[i]
+#     #이전인 i-1에서 A그룹에 elem을 더하는 경우
+#     for k in range(total+1):
+#         for j in range(total+1, elem-1, -1):
+#             if dp[i-1][j-elem][k]:
+#                 dp[i][j][k] = True
+
+#     #이전인 i-1에서 B그룹에 elem을 더하는 경우
+#     for j in range(total+1):
+#         for k in range(total+1, elem-1, -1):
+#             if dp[i-1][j][k-elem]:
+#                 dp[i][j][k] = True
+
+# if total%2 == 0 and dp[-1][total//2][total//2]:
+#     print("Yes")
+# else:
+#     print("No")
+
+
+dp = [[[False]*(total+1) for _ in range(total+1)] for _ in range(2)] #i까지 봐서 A의 합이 j, B의 합이 k
 # print(len(dp), len(dp[0]), len(dp[0][0]))
 dp[0][0][0] = True
 
-for i in range(1, n+1):
+for idx in range(n):
+    if idx%2 == 0:
+        i = 1
+    else:
+        i = 0
+
     for j in range(total+1):
         for k in range(total+1):
             if dp[i-1][j][k]:
                 dp[i][j][k] = True
     
-    elem = base[i]
+    elem = base[idx+1]
     #이전인 i-1에서 A그룹에 elem을 더하는 경우
     for k in range(total+1):
         for j in range(total+1, elem-1, -1):
