b047407a4113aa6f36070f1be859994fa411fac3
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-27 09:54:53 +0900

    Title: xor 결과 최대 만들기, Time: 314ms, Memory: 29MB, Status: Passed - CodeTree

--- 240227/xor 결과 최대 만들기/max-of-xor.py
+++ 240227/xor 결과 최대 만들기/max-of-xor.py
@@ -9,11 +9,11 @@ def sol(idx, count, total): #현재보는 base의 idx와 현재까지 선택한 
     if idx == n: #base를 다 봄
         return 0 #최댓값을 찾기에 없다는 의미로 0을 반환
     tmp = 0
-    #idx를 선택하는 경우
-    if count == 0: #처음에는 xor연산이 이뤄지지 않고 값이 그대로 들어감
-        tmp = max(tmp, sol(idx+1, count+1, base[idx]))
-    else:
-        tmp = max(tmp, sol(idx+1, count+1, total^base[idx]))
+    # #idx를 선택하는 경우
+    # if count == 0: #처음에는 xor연산이 이뤄지지 않고 값이 그대로 들어감
+    #     tmp = max(tmp, sol(idx+1, count+1, base[idx]))
+    # else:
+    tmp = max(tmp, sol(idx+1, count+1, total^base[idx])) #0과 숫자의 xor 연산의 결과는 숫자임!
     #idx를 선택하지 않는 경우
     tmp = max(tmp, sol(idx+1, count, total))
     return tmp
