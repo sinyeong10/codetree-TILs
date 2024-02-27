f2f8da7840c497ea91818f93f8f2904962c82d74
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-27 16:42:42 +0900

    Title: 삼 오 무, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240227/삼 오 무/three-five-moo.py
+++ 240227/삼 오 무/three-five-moo.py
@@ -3,16 +3,18 @@ n = int(stdin.readline())
 
 def check(num):
     return num//3+num//5-num//15 #3의 배수, 5의 배수는 Moo갯수 증가, 15의 배수는 중복으로 Moo갯수 감소!
+
 left = 1
 right = 10**9
+import sys
+min_value = sys.maxsize
 while left <= right:
     mid = (left+right)//2
     count = check(mid) #mid까지 Moo의 갯수
-    # print(left, right, mid, count, n)
+    # print(left, mid, right, count, mid-count, n)
     if mid-count < n: #mid-count는 숫자의 갯수
         left = mid+1 #이게 n보다 작으므로 mid는 우측으로 더 크게 봐야함!
-    elif mid-count > n:
+    else: #if mid-count >= n:
         right = mid-1
-    else:
-        break
-print(mid)
+        min_value = min(min_value, mid) #check(mid)에서 mid의 여러 값이 같은 결과로 나올 수 있음
+print(min_value)
