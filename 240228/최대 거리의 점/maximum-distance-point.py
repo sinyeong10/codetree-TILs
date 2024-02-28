bb3cbbdc31ae0ede56734cf02b628003299d73bf
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-28 18:48:03 +0900

    Title: 최대 거리의 점, Time: 251ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240228/최대 거리의 점/maximum-distance-point.py
@@ -0,0 +1,26 @@
+from sys import stdin
+n, m = list(map(int, stdin.readline().split()))
+point = [int(stdin.readline()) for _ in range(n)]
+
+import sys
+def check(dist): #dist가 최소거리일 때 m개이상이 가능
+    start = -sys.maxsize
+    cnt = 0
+    for i in range( n):
+        if point[i] >= start + dist:
+            start = point[i]
+            cnt += 1
+        # print(i, cnt)
+    return cnt >= m
+
+left = 1
+right = sys.maxsize
+ans = 0
+while left<=right:
+    mid = (left+right)//2
+    if check(mid): #
+        ans = max(ans, mid)
+        left = mid + 1
+    else:
+        right = mid - 1
+print(ans)
