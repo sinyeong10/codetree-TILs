eebd1c135ce84baa3b90046c19e1b655b6d83ce8
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-29 18:25:56 +0900

    Title: 겹치지 않는 선분위의 두 점, Time: 203ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 240229/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-don't-overlap.py
@@ -0,0 +1,32 @@
+from sys import stdin
+n, m = list(map(int, stdin.readline().split()))
+point = [list(map(int, stdin.readline().split())) for _ in range(m)]
+point.sort() #겹치지 않아서 처음 기준 정렬하면 충분!
+
+def check(dist):
+    count = 0
+    start = -dist #dist를 더해도 처음 값인 0이하가 되기 위해!
+    for i in range(m):
+        s,l = point[i]
+        if start + dist <= s: #이전 선분의 마지막 점의 위치로부터 dist의 거리가 현재 선분의 처음 위치 이하인 경우!
+            start = s
+            count += 1
+        #만약 앞의 if문이 안걸렸다면 현재 선분의 시작위치에서 시작 못함!
+        while start+dist <= l: #최대 l까지 dist씩 계속 이동!
+            start += dist
+            count += 1
+    # print(count)
+    return count >= n
+
+left = 1
+import sys
+right = sys.maxsize
+min_value = sys.maxsize
+while left<=right:
+    mid = (left+right)//2
+    if check(mid): #n개 이상이므로 dist를 더 크게 해서 다시 체크!
+        min_value = min(min_value, mid)
+        left = mid+1
+    else:
+        right = mid-1
+print(min_value)
