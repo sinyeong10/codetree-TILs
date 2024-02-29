dbb7f39b2af7d6360fe12a64f0c776a9eaee2f4f
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-29 18:42:12 +0900

    Title: 겹치지 않는 선분위의 두 점, Time: 970ms, Memory: 27MB, Status: Passed - CodeTree

--- 240229/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-don't-overlap.py
+++ 240229/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-don't-overlap.py
@@ -3,28 +3,42 @@ n, m = list(map(int, stdin.readline().split()))
 point = [tuple(map(int, stdin.readline().split())) for _ in range(m)]
 point.sort() #겹치지 않아서 처음 기준 정렬하면 충분!
 
-def check(dist):
-    count = 1
-    # start = -dist #dist를 더해도 처음 값인 0이하가 되기 위해!
-    start = point[0][0] #처음을 이걸로 해서 선택된 후에 시작!
-    for s,l in point:
-        if start + dist > l: #현재 선분내에서는 불가능!
-            continue
-        if start + dist <= s: #이전 선분의 마지막 점의 위치로부터 dist의 거리가 현재 선분의 처음 위치 이하인 경우!
-            start = s
-            count += 1
 
-        # #만약 앞의 if문이 안걸렸다면 현재 선분의 시작위치에서 시작 못함!
-        # while start+dist <= l: #최대 l까지 dist씩 계속 이동!
-        #     start += dist
-        #     count += 1
-        tmp = (l-start)//dist
-        if tmp > 0:
-            count += tmp
-            start += tmp*dist
-        if count >= n:
-            return True
-    return False
+def check(dist):
+    cnt = 1
+    prev = point[0][0]
+    for start, end in point:
+        if prev + dist > end:
+            continue
+        if prev + dist < start:
+            prev = start
+            cnt += 1
+        dots = max((end - prev) // dist, 0)
+        cnt += dots
+        prev += dots * dist
+    return cnt >= n
+
+# def check(dist):
+#     count = 1
+#     # start = -dist #dist를 더해도 처음 값인 0이하가 되기 위해!
+#     start = point[0][0] #처음을 이걸로 해서 선택된 후에 시작!
+#     for s,l in point:
+#         if start + dist > l: #현재 선분내에서는 불가능!
+#             continue
+#         if start + dist <= s: #이전 선분의 마지막 점의 위치로부터 dist의 거리가 현재 선분의 처음 위치 이하인 경우!
+#             start = s
+#             count += 1
+#         # #만약 앞의 if문이 안걸렸다면 현재 선분의 시작위치에서 시작 못함!
+#         # while start+dist <= l: #최대 l까지 dist씩 계속 이동!
+#         #     start += dist
+#         #     count += 1
+#         tmp = (l-start)//dist
+#         if tmp > 0:
+#             count += tmp
+#             start += tmp*dist
+#         if count >= n:
+#             return True
+#     return False
 
 left = 1
 # import sys
