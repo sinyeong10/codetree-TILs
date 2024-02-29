f36d23493dcaabf02635e79926b4d84dda2d8a69
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-02-29 17:54:42 +0900

    Title: 최소 통과 시간, Time: 207ms, Memory: 25MB, Status: Passed - CodeTree

--- 240229/최소 통과 시간/minimum-transit-time.py
+++ 240229/최소 통과 시간/minimum-transit-time.py
@@ -8,9 +8,11 @@ def check(time):
         total += time // elem #각 통로를 시간동안 모두 최대한 이용하는 경우
     return total >= n
 
+#1000000000는 통로를 통과하는 최대 시간, 같은 통로를 한번만 이용하는 것이 아니기에 전체 시간은 더 걸릴 수 있음!
+import sys
 left = 1
-right = 1000000000
-min_value = 1000000000
+right = sys.maxsize
+min_value = sys.maxsize
 while left<=right:
     mid = (left+right)//2
     if check(mid): #가능 시간을 더 줄여서 최소로 만듦!
