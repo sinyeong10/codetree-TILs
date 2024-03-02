db1c8509c8ca292c6c715632dcc92c0d9f8e544f
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-03-02 13:12:30 +0900

    Title: 격자 칠하기 2, Time: 138ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240302/격자 칠하기 2/painting-the-grid-2.py
@@ -0,0 +1,57 @@
+from sys import stdin
+n = int(stdin.readline())
+base_2d = [list(map(int, stdin.readline().split())) for _ in range(n)]
+visited = [[False]*n for _ in range(n)]
+
+def in_range(i, j):
+    return 0<=i<n and 0<=j<n
+
+#bfs 시간초과
+from collections import deque
+def bfs(i, j, D):
+    # print(i,j,D)
+    # #처음 상태 초기화!
+    # visited = [[False]*n for _ in range(n)]
+
+    q = deque()
+    q.append((i,j))
+    visited[i][j] = True
+
+    dx, dy =[0,0,-1,1], [-1,1,0,0]
+    
+    total = 1
+
+    while q:
+        x, y = q.popleft()
+        for dxs, dys in zip(dx, dy):
+            next_x, next_y = x+dxs, y+dys
+            if in_range(next_x, next_y) and not visited[next_x][next_y] and abs(base_2d[x][y]-base_2d[next_x][next_y])<=D: #범위안이고 방문을 아직 안한 경우이며 현재 값보다 D이하로 차이나야함!
+                q.append((next_x, next_y))
+                total += 1
+                visited[next_x][next_y] = True
+    return total
+
+def check(D):
+    global visited
+    visited = [[False]*n for _ in range(n)]
+    ans = 0
+    for i in range(n):
+        for j in range(n):
+            if not visited[i][j]:
+                # print(D, i, j, visited[i][j])
+                ans = max(ans, bfs(i,j,D))
+                if ans >= (n*n+1)//2:
+                    return True #반올림 처리!
+    return False
+
+left = 1
+right = 1000000
+min_value = 1000000
+while left <= right:
+    mid = (left+right)//2
+    if check(mid):
+        min_value = min(min_value, mid)
+        right = mid-1
+    else:
+        left = mid+1
+print(min_value)
