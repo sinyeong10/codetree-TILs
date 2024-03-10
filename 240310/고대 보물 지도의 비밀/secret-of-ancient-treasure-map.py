70bffb7897f0ecaf8c6877a6f6129fd2b337ac86
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-03-10 14:54:48 +0900

    Title: 고대 보물 지도의 비밀, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240310/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
+++ 240310/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
@@ -7,15 +7,18 @@ initial = -100#sys.maxsize
 dp = [[initial]*(k+1) for _ in range(n)] #i번째까지 봄, j개의 음수
 #k에서 아래 방향 계산만 필요한 데 범위 게산을 하지 않기 위해서 k+1까지 인덱스 생성!
 
+#처음 초기화
 dp[0][0] = base[0] if base[0] >= 0 else initial
 dp[0][1] = base[0] if base[0] < 0 else initial
 
 for i in range(1, n):
     if base[i] > 0: #양수면 누적해서 더함
-        if dp[i-1][0] != initial: #이전이 계산 가능시 누적
-            dp[i][0] = dp[i-1][0] + base[i]
-        else: #이전이 불가능시 새로!
-            dp[i][0] = base[i]
+        # if dp[i-1][0] != initial: #이전이 계산 가능시 누적
+        #     dp[i][0] = dp[i-1][0] + base[i]
+        # else: #이전이 불가능시 새로!
+        #     dp[i][0] = base[i]
+        #이전의 값이 없으면 -라 base[i]가 무조건 더 큼
+        dp[i][0] = max(dp[i-1][0]+base[i], base[i])
 
 for i in range(1, n):
     for j in range(1, k+1):
