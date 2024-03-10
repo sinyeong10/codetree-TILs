d3a507d1c479dd20a03e95317e0a2ba9a7f1d331
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-03-10 23:39:21 +0900

    Title: 게임 스튜디오에서 살아남기, Time: 149ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240310/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.py
@@ -0,0 +1,31 @@
+from sys import stdin
+n = int(stdin.readline())
+
+dp = [[0]*4 for _ in range(n)]
+
+total = 0
+ans = []
+
+def sol(idx, cnt):
+    global total
+    if idx == n:
+        total += 1
+        return
+    
+    ans.append("G")
+    sol(idx+1, cnt)
+    ans.pop()
+
+    if cnt < 2: #2번까지 가능!
+        ans.append("T")
+        sol(idx+1, cnt+1)
+        ans.pop()
+
+    if len(ans) > 2 and ans[-1] == "B" and ans[-2] == "B":
+        return
+    ans.append("B")
+    sol(idx+1, cnt)
+    ans.pop()
+
+sol(0, 0)
+print(total%(10**9+7))
