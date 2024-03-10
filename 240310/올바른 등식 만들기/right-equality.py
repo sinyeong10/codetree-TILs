ace0fe90f74a484ad19dbf23ad307eb99cce95d2
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-03-10 16:24:45 +0900

    Title: 올바른 등식 만들기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240310/올바른 등식 만들기/right-equality.py
@@ -0,0 +1,33 @@
+from sys import stdin
+n, m = list(map(int, stdin.readline().split()))
+base = list(map(int, stdin.readline().split()))
+
+offset = 20 #-20+offset=0이고 20+offset은 40이 되야함!
+dp = [[0]*41 for _ in range(n)] #i번째까지 보고 j-offset의 값임
+
+for i in range(n):
+    dp[i][base[i]+offset] += 1
+    dp[i][-base[i]+offset] += 1
+
+for i in range(1, n):
+    for j in range(base[i], 41):
+        if dp[i-1][j-base[i]] != 0:
+            dp[i][j] += dp[i-1][j-base[i]]
+    
+    for j in range(41-base[i]):
+        if dp[i-1][j+base[i]] != 0:
+            dp[i][j] += dp[i-1][j+base[i]]
+
+
+# for elem in dp:
+#     print(*elem)
+
+# for i in range(n):
+#     for j in range(41):
+#         if dp[i][j] > 0:
+#             print(i, j-offset, dp[i][j])
+
+max_value = 0
+for i in range(n):
+    max_value = max(max_value, dp[i][m+offset])
+print(max_value)
