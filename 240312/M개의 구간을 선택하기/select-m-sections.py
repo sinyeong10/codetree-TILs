06e543a46af62a5f5b8692348ce4f52f0554fd94
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-03-12 11:04:37 +0900

    Title: M개의 구간을 선택하기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240312/M개의 구간을 선택하기/select-m-sections.py
+++ 240312/M개의 구간을 선택하기/select-m-sections.py
@@ -7,30 +7,32 @@ import sys
 def sol(idx, cnt):
     total = -sys.maxsize
 
-    if cnt == m:
+    if cnt == m-1: #m-1가 되고 바로 끝나면 안됨!
         total = 0
         for i in range(len(ans)):
             total += ans[i]
         # print("값", idx, cnt, ans, total)
+        if idx < n: #범위 끝까지 값을 추가해 봄
+            ans.append(base[idx])
+            total = max(total, sol(idx+1, cnt))
+            ans.pop()
         return total
 
-    if cnt > m or idx >= n:
+    if cnt > m or idx >= n: #범위 초과
         # print("멈춤", idx, cnt, ans)
         return total
     # print(idx, cnt, ans)
     
-
-    # 현재 인덱스 선택후 계속 선택
-    ans.append(base[idx])
+    #현재 값 선택 안하고 넘어감!
     total = max(total, sol(idx+1, cnt))
 
-    #현재 인덱스 선택 후 종료
-    total = max(total, sol(idx+2,cnt+1))
+    # 현재 인덱스 선택
+    if ans and ans[-1] != base[idx-1]: #이전 값이 있고, 현재 선택 바로 전의 값이 아닌 경우
+        cnt += 1
+    ans.append(base[idx])
+    total = max(total, sol(idx+1, cnt))
     ans.pop()
 
-    #현재 인덱스 선택 안하고 넘어감!
-    if ans:
-        total = max(total, sol(idx+1, cnt+1))
     return total
 
 print(sol(0,0))
