7ee9129949738984818c602c2a8d4823617cdb8a
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-03-12 10:29:29 +0900

    Title: M개의 구간을 선택하기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240312/M개의 구간을 선택하기/select-m-sections.py
@@ -0,0 +1,34 @@
+from sys import stdin
+n, m = list(map(int, stdin.readline().split()))
+base = list(map(int, stdin.readline().split()))
+
+ans = []
+def sol(idx, cnt):
+    total = 0
+
+    if cnt == m:
+        for i in range(len(ans)):
+            total += ans[i]
+        # print("값", idx, cnt, ans, total)
+        return total
+
+    if cnt > m or idx >= n:
+        # print("멈춤", idx, cnt, ans)
+        return total
+    # print(idx, cnt, ans)
+    
+
+    # 현재 인덱스 선택후 계속 선택
+    ans.append(base[idx])
+    total = max(total, sol(idx+1, cnt))
+
+    #현재 인덱스 선택 후 종료
+    total = max(total, sol(idx+2,cnt+1))
+    ans.pop()
+
+    #현재 인덱스 선택 안하고 넘어감!
+    if ans:
+        total = max(total, sol(idx+1, cnt+1))
+    return total
+
+print(sol(0,0))
