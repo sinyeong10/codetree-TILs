137ed9985b9c212ad6bb235921ff2bdd5a82d258
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-03-13 13:36:40 +0900

    Title: 두 수의 합, Time: 131ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 240313/두 수의 합/sum-of-two-num.py
+++ 240313/두 수의 합/sum-of-two-num.py
@@ -13,7 +13,7 @@ for i in range(n):
 total = 0
 for elem in num_count.keys():
     if k-elem == elem: #같은 값인 경우은 1번만 불려짐
-        total += 2 #순서 상관없어서 중복을 2로 나눠서 처리!
+        total += num_count[elem]*(num_count[elem]-1) #1번 불려질 때 n개라면 nC2만큼 가능!
     elif k-elem in num_count: #다른 값인 경우은 k-elem, elem으로 숫자가 2번 불려짐
         total += 1
-print(total // 2)
+print(total // 2) #이후 중복을 2로 나눠서 처리
