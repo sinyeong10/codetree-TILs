d73f687876362be90af0e2a153de9352651f4ceb
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-03-16 13:56:04 +0900

    Title: 특별한 문자, Time: 166ms, Memory: 25MB, Status: Passed - CodeTree

--- 240316/특별한 문자/special-character.py
+++ 240316/특별한 문자/special-character.py
@@ -10,16 +10,16 @@ for elem in word:
 
 flag = True
 #딕셔너리가 입력 순서대로 저장되는 것이 보장될 시
-# for key, value in count.items():
-#     if flag and value == 1:
-#         flag = False
-#         print(key)
-
-#word로 순회해도 됨!
-for elem in word:
-    if flag and count[elem] == 1:
+for key, value in count.items():
+    if flag and value == 1:
         flag = False
-        print(elem)
+        print(key)
+
+# #word로 순회해도 됨!
+# for elem in word:
+#     if flag and count[elem] == 1:
+#         flag = False
+#         print(elem)
 
 
 if flag: #앞에서 출력이 안됨!
