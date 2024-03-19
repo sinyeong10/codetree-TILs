c30042e99c788c9bc20bf2e2daa2ab31331c85c1
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-03-19 09:08:18 +0900

    Title: C-TAG, Time: 1569ms, Memory: 27MB, Status: Passed - CodeTree

---
+++ 240319/C-TAG/c-tag.py
@@ -0,0 +1,43 @@
+from sys import stdin
+n, m = list(map(int, stdin.readline().split()))
+A = [stdin.readline() for _ in range(n)]
+B = [stdin.readline() for _ in range(n)]
+
+total = 0
+def check(idxs):
+    global total
+    tmpA = set()
+    for i in range(n):
+        key = ""
+        for j in idxs:
+            key += A[i][j]
+        tmpA.add(key)
+    
+    for i in range(n):
+        key = ""
+        for j in idxs:
+            key += B[i][j]
+        if key in tmpA:
+            return 0
+    total += 1
+    return 1
+
+idxs = []
+def find_idx(idx, cnt):
+    if cnt > 4 or idx > m:
+        return
+
+    if cnt == 3:
+        check(idxs)
+        # if check(idxs):
+            # print(idxs)
+        return
+
+
+    find_idx(idx+1, cnt)
+    idxs.append(idx)
+    find_idx(idx+1, cnt+1)
+    idxs.pop()
+
+find_idx(0,0)
+print(total)
