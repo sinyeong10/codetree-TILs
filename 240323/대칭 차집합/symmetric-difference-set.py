aa13543af5c3d1d27da622e1d31f2b47a4e30a30
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-03-23 19:33:10 +0900

    Title: 대칭 차집합, Time: 231ms, Memory: 63MB, Status: Passed - CodeTree

---
+++ 240323/대칭 차집합/symmetric-difference-set.py
@@ -0,0 +1,16 @@
+from sys import stdin
+a, b = list(map(int, stdin.readline().split()))
+A = set(map(int, stdin.readline().split()))
+B = set(map(int, stdin.readline().split()))
+# print(A,B)
+
+ans = 0
+for elem in A: #set은 인덱스 순회 불가능!
+    if elem not in B: #A-B
+        ans += 1
+
+for elem in B:
+    if elem not in A: #B-A
+        ans += 1
+
+print(ans)
