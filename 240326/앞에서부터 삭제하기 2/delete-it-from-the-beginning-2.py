d11d9edd168c3ac5b977bf6ca3349a5575fde5c9
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-03-26 09:50:50 +0900

    Title: 앞에서부터 삭제하기 2, Time: 311ms, Memory: 32MB, Status: Passed - CodeTree

---
+++ 240326/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -0,0 +1,26 @@
+from sys import stdin
+
+n = int(stdin.readline())
+base = list(map(int, stdin.readline().split()))
+
+import heapq
+ans = 0
+total = base[-1]
+hq = []
+heapq.heappush(hq, base[-1])
+
+for i in range(n-2, 0, -1):
+    heapq.heappush(hq, base[i])
+    total += base[i]
+
+    #최솟값 제거
+    tmp = heapq.heappop(hq)
+    total -= tmp
+    # print(base[i], total, len(hq), total/len(hq))
+    #평균 계산
+    ans = max(ans, total/len(hq))
+    #다시 복구
+    heapq.heappush(hq, tmp)
+    total += tmp
+
+print(f"{ans:.2f}")
