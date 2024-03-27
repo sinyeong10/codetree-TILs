4f2b88a5a74ce10dfda30edbd5c548e76ae83a13
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-03-27 15:54:10 +0900

    Title: 중앙값, Time: 589ms, Memory: 35MB, Status: Passed - CodeTree

--- 240327/중앙값/median.py
+++ 240327/중앙값/median.py
@@ -13,25 +13,28 @@ for _ in range(t):
             heapq.heappush(a, -num)
 
         elif len(a) == len(b): #a에 추가!
-            b_min = heapq.heappop(b)
-            if b_min >= num:
+            #b[0]가 b_min임
+            # b_min = heapq.heappop(b)
+            if b[0] >= num:
                 heapq.heappush(a, -num)
-                heapq.heappush(b, b_min)
+                # heapq.heappush(b, b_min)
             else:
+                b_min = heapq.heappop(b)
                 heapq.heappush(a, -b_min)
                 heapq.heappush(b, num)
 
         elif len(a) > len(b): #b에 추가
-            a_max = -heapq.heappop(a)
-            if a_max < num:
+            # a_max = -heapq.heappop(a)
+            if -a[0] < num:
                 heapq.heappush(b, num)
-                heapq.heappush(a, -a_max)
+                # heapq.heappush(a, -a_max)
             else:
+                a_max = -heapq.heappop(a)
                 heapq.heappush(a, -num)
                 heapq.heappush(b, a_max)
 
         if i % 2 == 0:
-            tmp = -heapq.heappop(a)
-            print(tmp, end=" ")
-            heapq.heappush(a, -tmp)
+            # tmp = -heapq.heappop(a)
+            print(-a[0], end=" ")
+            # heapq.heappush(a, -tmp)
     print()
