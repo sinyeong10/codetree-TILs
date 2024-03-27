48ee3dac8d2ea8a6e327cb7784e3c5b9dd631af5
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-03-27 16:57:50 +0900

    Title: k번째로 작은 쌍의 합, Time: 736ms, Memory: 39MB, Status: Passed - CodeTree

--- 240327/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
+++ 240327/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
@@ -1,13 +1,32 @@
 from sys import stdin
 n, m, k = list(map(int, stdin.readline().split()))
-first = list(map(int, stdin.readline().split()))
-second = list(map(int, stdin.readline().split()))
+first = sorted(list(map(int, stdin.readline().split())))
+second = sorted(list(map(int, stdin.readline().split())))
 
-total = []
+#메모리 초과
+# total = []
+# for i in range(n):
+#     for j in range(m):
+#         total.append((first[i], second[j]))
+
+# total.sort(key=lambda x:(x[0]+x[1]))
+# a,b=total[k-1]
+# print(a+b)
+
+find_index= {}
 for i in range(n):
-    for j in range(m):
-        total.append((first[i], second[j]))
+    find_index[first[i]] = i
 
-total.sort(key=lambda x:(x[0]+x[1]))
-a,b=total[k-1]
-print(a+b)
+import heapq
+hq = []
+
+for j in range(m):
+    heapq.heappush(hq, (first[0]+second[j], first[0], second[j]))
+
+for _ in range(k):
+    ans, key, tmp = heapq.heappop(hq)
+    idx = find_index[key]
+    if idx != n-1: #마지막이 아니면 다음으로 가서 삽입
+        heapq.heappush(hq, (first[idx+1]+tmp, first[idx+1], tmp))
+    # print(key,tmp, hq)
+print(ans)
