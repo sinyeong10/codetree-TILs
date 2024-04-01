53b27ce37a556511ac47590fb1895f435425721d
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-04-01 19:18:55 +0900

    Title: 초대장과 번호표, Time: 253ms, Memory: 38MB, Status: Passed - CodeTree

--- 240401/초대장과 번호표/invitation-and-number-tag.py
+++ 240401/초대장과 번호표/invitation-and-number-tag.py
@@ -67,32 +67,33 @@ n, g = list(map(int, stdin.readline().split()))
 
 
 
-group = [set(list(map(int, stdin.readline().split()))[1:]) for _ in range(g)]
-# print(group)
+people_groups = [[] for _ in range(n)] #각 사람이 속하는 그룹을 의미
+groups = [set() for _ in range(g)]
 
-ans = {1}
+
 for i in range(g):
-    if 1 in group[i]:
-        group[i].remove(1)
+    nums = list(map(int, stdin.readline().split()))[1:]
+    for num in nums:
+        num -= 1 #인덱스단위로 변환
+        groups[i].add(num)
+        people_groups[num].append(i)
+# print(groups, people_groups)
 
-# from collections import deque #tmp를 리스트가 아닌 큐로 관리
-# tmp = deque()
-# tmp.append(1)
-tmp = {1}
-while tmp:
-    # while tmp:
-    key = list(tmp)[0]
-    # key = tmp.popleft()
-    ans.add(key)
-    tmp.remove(key)
-    for i in range(g):
-        if key in group[i]:
-            group[i].remove(key)
+visited = {0}
+q = {0}
 
-    for i in range(g):
-        if len(group[i]) == 1:
-            tmp.add(list(group[i])[0])
-            # tmp.append(list(group[i])[0])
-    # print(tmp)
-# print(group)
-print(len(ans))
+while q:
+    key = list(q)[0]
+    visited.add(key)
+    q.remove(key)
+
+    for g_num in people_groups[key]: #key가 포함된 그룹
+        # print(g_num, key, people_groups[key])
+        groups[g_num].remove(key)
+        #삭제후 1명이면 초대장을 받음
+        if len(groups[g_num]) == 1:
+            p_num = list(groups[g_num])[0]
+            if p_num not in visited:
+                visited.add(p_num)
+                q.add(p_num)
+print(len(visited))
