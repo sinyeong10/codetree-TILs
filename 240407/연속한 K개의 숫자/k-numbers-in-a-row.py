fe4db9edd86a9b182ca1554ac34642b19da67482
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-04-07 17:59:03 +0900

    Title: 연속한 K개의 숫자, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240407/연속한 K개의 숫자/k-numbers-in-a-row.py
@@ -0,0 +1,20 @@
+from sys import stdin
+n, k, b = list(map(int, stdin.readline().split()))
+base = [0 for _ in range(n+1)]
+for _ in range(b):
+    num = int(stdin.readline())
+    base[num] = 1 #빠진 수
+
+# print(base)
+
+prefix = [0 for _ in range(n+1)] #현재 숫자까지 연속이 되기 위해 필요한 숫자
+for i in range(n): #base의 값이 1부터 시작인데 0부터 있어서 i+1로 해야함!
+    prefix[i+1] = prefix[i]+base[i+1]
+
+# print(prefix)
+
+min_value = b
+for i in range(n-k):
+    # print(f"{i+1}~{i+k}까지 연속되기 위해 필요한 수 : {prefix[i+k]-prefix[i]}")
+    min_value = min(min_value, prefix[i+k]-prefix[i]) #i+1부터 i+k까지 k개의 숫자 체크
+print(min_value)
