19906cf14b1ce613a603b5d1eb10c545e445a433
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-04-07 18:00:50 +0900

    Title: 연속한 K개의 숫자, Time: 132ms, Memory: 26MB, Status: Passed - CodeTree

--- 240407/연속한 K개의 숫자/k-numbers-in-a-row.py
+++ 240407/연속한 K개의 숫자/k-numbers-in-a-row.py
@@ -14,7 +14,7 @@ for i in range(n): #base의 값이 1부터 시작인데 0부터 있어서 i+1로
 # print(prefix)
 
 min_value = b
-for i in range(n-k):
+for i in range(n-k+1): #인덱스가 숫자를 의미해서 범위가 n까지임!
     # print(f"{i+1}~{i+k}까지 연속되기 위해 필요한 수 : {prefix[i+k]-prefix[i]}")
     min_value = min(min_value, prefix[i+k]-prefix[i]) #i+1부터 i+k까지 k개의 숫자 체크
 print(min_value)
