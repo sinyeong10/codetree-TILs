5295f029c00ee9d2fed86db83603c03210c66700
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-04-15 13:56:24 +0900

    Title: 겹치는 선분들, Time: 737ms, Memory: 33MB, Status: Passed - CodeTree

---
+++ 240415/겹치는 선분들/overlapping-lines.py
@@ -0,0 +1,34 @@
+from sys import stdin
+n, k = list(map(int, stdin.readline().split()))
+base = 0
+point = []
+#0~2가 2가 되기 위해 [,)로 인식!
+for _ in range(n):
+    m1, m2 = stdin.readline().split()
+    if m2 == "R":
+        point.append((base, +1))
+        base += int(m1)
+        point.append((base, -1))
+    else:
+        point.append((base, -1))
+        base -= int(m1)
+        point.append((base, +1))
+point.sort()
+# print(point)
+
+ans = 0
+total = 0
+last = point[0][0]
+for i in range(2*n):
+    if total >= k: #현재 점의 상태를 보고 k이상이면 이전 값과의 차이를 가산
+        ans += point[i][0]-point[i-1][0]
+    total += point[i][1] #상태 갱신
+
+    # if total == k-1 and point[i][1] == -1: #처음 내려갈 때 계산
+    #     ans += abs(last-point[i][0])
+    #     last = point[i][0]
+    # elif total == k and point[i][1] == 1: #처음 올라갈때 시작 위치 기록
+    #     last = point[i][0]
+    # # print(total, last, point[i], ans)
+
+print(ans)
