07d47afc4cd274d07148d613ff57dd89a41d64e1
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-04-16 13:10:29 +0900

    Title: 컴퓨터 이용시간, Time: 1038ms, Memory: 45MB, Status: Passed - CodeTree

---
+++ 240416/컴퓨터 이용시간/computer-hours.py
@@ -0,0 +1,35 @@
+from sys import stdin
+n = int(stdin.readline())
+
+seat = [0 for _ in range(n+1)]
+find_seat = [False for _ in range(n)]
+
+import heapq
+#min-heaq
+order = []
+
+for i in range(n):
+    heapq.heappush(order, i+1)
+
+#heapq.heappop(order)
+
+point = []
+for i in range(n):
+    p, q = list(map(int, stdin.readline().split()))
+    point.append((p, +1, i))
+    point.append((q, -1, i))
+
+point.sort()
+
+for value, direct, idx in point:
+    # print("\n", value, direct, idx)
+    if direct == 1:
+        key = heapq.heappop(order)
+        seat[key] = idx
+        find_seat[idx] = key
+    else:
+        heapq.heappush(order, find_seat[idx])
+        # print(find_seat[idx])
+    # print(order)
+
+print(*find_seat)
