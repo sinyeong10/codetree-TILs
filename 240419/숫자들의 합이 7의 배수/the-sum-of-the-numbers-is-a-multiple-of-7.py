8c552fae51285c67fbb9869ad4821f87fd744bd7
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-04-19 11:18:45 +0900

    Title: 숫자들의 합이 7의 배수, Time: 135ms, Memory: 26MB, Status: Passed - CodeTree

--- 240419/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.py
+++ 240419/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.py
@@ -8,10 +8,21 @@ for i in range(n):
 # print(base)
 # print(prefix)
 
+#시간초과
+# max_value = 0
+# for i in range(1,n):
+#     for j in range(i+1, n):
+#         if prefix[j]-prefix[i-1] == 0: #(prefix[j]+7-prefix[i-1])%7 == 0:
+#             max_value = max(max_value, j-i+1)
+#             # print(j, i, j-i+1) #2~1은 2임
+# print(max_value)
+
 max_value = 0
-for i in range(1,n):
-    for j in range(i+1, n):
-        if prefix[j]-prefix[i-1] == 0: #(prefix[j]+7-prefix[i-1])%7 == 0:
-            max_value = max(max_value, j-i+1)
-            # print(j, i, j-i+1) #2~1은 2임
+record = {}
+for i in range(n):
+    if prefix[i] not in record: #처음 인덱스 기록!
+        record[prefix[i]] = i
+    else: #처음과 현재 인덱스의 차가 현재까지 고른 숫자의 합이 7의 배수인 경우!
+        max_value = max(max_value, i-record[prefix[i]])
+
 print(max_value)
