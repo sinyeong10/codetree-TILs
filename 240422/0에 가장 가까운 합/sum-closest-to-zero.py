fa5b8eeda8715bec7452ad003540014b61da762e
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-04-22 15:18:46 +0900

    Title: 0에 가장 가까운 합, Time: 167ms, Memory: 31MB, Status: Passed - CodeTree

--- 240422/0에 가장 가까운 합/sum-closest-to-zero.py
+++ 240422/0에 가장 가까운 합/sum-closest-to-zero.py
@@ -2,12 +2,13 @@ from sys import stdin
 n = int(stdin.readline())
 base = list(map(int, stdin.readline().split()))
 base.sort()
+# print(base)
 
 import sys
 min_value = sys.maxsize
+j = n-1
 for i in range(n):
-    j = n-1
-    while j>i+1 and abs(base[i]+base[j])>abs(base[i]+base[j-1]):
+    while j>i+1 and abs(base[i]+base[j])>=abs(base[i]+base[j-1]): #등호를 포함하지 않으면 같은 값이면 멈춤...
         j -= 1
     # print(i,j, base[i]+base[j])
     if j>i:
