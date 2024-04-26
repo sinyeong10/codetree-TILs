83efd617351eed40db4f5c42ad0c83afd6e5448d
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-04-26 20:24:48 +0900

    Title: 중복되지 않는 가장 긴 문자열, Time: 144ms, Memory: 25MB, Status: Passed - CodeTree

--- 240426/중복되지 않는 가장 긴 문자열/longest-not-duplicated-substring.py
+++ 240426/중복되지 않는 가장 긴 문자열/longest-not-duplicated-substring.py
@@ -1,9 +1,21 @@
 from sys import stdin
 base = stdin.readline().strip()
-# tmp = set()
-# ans = 0
-# end = 0
-# for i in range(len(base)):
-#     if base[i] not in tmp:
-tmp = set(base)
-print(len(tmp))
+tmp = set()
+ans = 0
+total = 0
+end = 0
+for i in range(len(base)):
+    while end < len(base) and base[end] not in tmp:
+        tmp.add(base[end])
+        end += 1
+        total += 1
+    if end <= len(base): #인덱스가 허용된건지 체크
+        # print(tmp)
+        ans = max(ans, total)
+    tmp.remove(base[i])
+    total -= 1
+print(ans)
+
+# #연속부분문자열
+# tmp = set(base)
+# print(len(tmp))
