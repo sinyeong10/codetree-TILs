09da9dba338ba7555bdbf4ad3ff40932dd85fdf0
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-04-30 14:08:55 +0900

    Title: 1이 k개 이상 존재하는 부분 수열, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240430/1이 k개 이상 존재하는 부분 수열/subsequence-with-k-or-more-1's.py
@@ -0,0 +1,21 @@
+from sys import stdin
+n, k = list(map(int, stdin.readline().split()))
+base = list(map(int, stdin.readline().split()))
+
+import sys
+ans = sys.maxsize
+count = 0
+end = 0
+total = 0
+for first in range(n):
+    while end < n and count < k: #end가 n-1이면 체크 가능, n이면 체크 불가능!
+        if base[end] == 1:
+            count += 1
+        end += 1
+        total += 1
+    # print(base[first:end], first, end, count, total)
+    if count >= k:
+        ans = min(ans, total)
+        count -= 1
+    total -= 1
+print(ans if ans != sys.maxsize else -1)
