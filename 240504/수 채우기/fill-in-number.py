8e99c50d9f4a41ef1bc2c673ce6e7597d1aa5a4a
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-05-04 20:02:39 +0900

    Title: 수 채우기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240504/수 채우기/fill-in-number.py
@@ -0,0 +1,12 @@
+from sys import stdin
+n = int(stdin.readline())
+
+import sys
+dp = [sys.maxsize for _ in range(n+1)]
+for i in range(n+1):
+    if dp[i] > 0: #가능한 경우만!
+        if i+2 <= n:
+            dp[i+2] = min(dp[i+2], dp[i]+1)
+        if i+5 <= n:
+            dp[i+5] = min(dp[i+5], dp[i]+1)
+print(dp[n] if dp[n]!=sys.maxsize else -1)
