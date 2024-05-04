2d8578550a05200ffa30abfd9aac5fbcf3684b20
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-05-04 20:01:38 +0900

    Title: 회의실 겹치지 않게 하기, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240504/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
@@ -0,0 +1,16 @@
+from sys import stdin
+n = int(stdin.readline())
+base = [list(map(int, stdin.readline().split())) for _ in range(n)]
+
+base.sort(key=lambda x : (x[1],x[0]))
+print(base)
+
+#특정 시간을 종료시점으로 잡았을 때 그때까지 가장 많은 회의를 하는 경우
+ans = 0
+last = -1
+for i in range(n):
+    a,b = base[i]
+    if last <= a:
+        ans += 1
+        last = b
+print(n-ans)
