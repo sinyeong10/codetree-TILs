1853af7ab80a6932f25ba298bda5282712752016
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-05-05 10:49:25 +0900

    Title: 계단 수, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240505/계단 수/stair-number.py
+++ 240505/계단 수/stair-number.py
@@ -10,7 +10,7 @@ for j in range(1, 10):
 divide_value = 10**9+7
 
 for i in range(1, n):
-    for j in range(10):
+    for j in range(10): #굳이 이전에 가능했는지 체크안해도 됨! 0인 경우는 0이고 다 값이 있음
         if j != 0 and dp[i-1][j-1] > 0: #이전에 가능했다면
             dp[i][j] += dp[i-1][j-1]
         if j != 9 and dp[i-1][j+1] > 0:
