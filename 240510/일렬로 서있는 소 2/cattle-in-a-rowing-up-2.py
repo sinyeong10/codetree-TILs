02fb8b4b41bfe755ab43acbbe518dd1f7c5f6ce9
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-05-10 10:35:54 +0900

    Title: 일렬로 서있는 소 2, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240510/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240510/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -2,10 +2,36 @@ from sys import stdin
 n = int(stdin.readline())
 base = list(map(int, stdin.readline().split()))
 
+#완전탐색
+# cnt = 0
+# for i in range(n):
+#     for j in range(i+1, n):
+#         for k in range(j+1, n):
+#             if base[i] <= base[j] <= base[k]:
+#                 cnt += 1
+# print(cnt)
+
+L = [0 for _ in range(n)]
+for i in range(n):
+    tmp = 0
+    for j in range(i):
+        if base[j] < base[i]:
+            tmp += 1
+    L[i] = tmp
+# print(L)
+
+
+R = [0 for _ in range(n)]
+for i in range(n-1,-1,-1):
+    tmp = 0
+    for j in range(i+1, n):
+        if base[j] > base[i]:
+            tmp += 1
+    R[i] = tmp
+# print(R)
+
+
 cnt = 0
 for i in range(n):
-    for j in range(i+1, n):
-        for k in range(j+1, n):
-            if base[i] <= base[j] <= base[k]:
-                cnt += 1
+    cnt += L[i] * R[i]
 print(cnt)
