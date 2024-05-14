20a3e8c48c9884ef8d68b1f136bfbfa0b9e30ad2
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-05-14 11:37:39 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240514/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240514/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -1,9 +1,24 @@
+#완전 탐색
+# from sys import stdin
+# base = stdin.readline().strip()
+# n = len(base)
+# cnt = 0
+# for i in range(n-1):
+#     for j in range(i+2, n-1):
+#         if base[i]==base[i+1]=="(" and base[j]==base[j+1]==")":
+#             cnt += 1
+# print(cnt)
+
+#한방향 처리!
 from sys import stdin
-base = stdin.readline().strip()
-n = len(base)
+A = stdin.readline().strip()
+n = len(A)
+
 cnt = 0
-for i in range(n-1):
-    for j in range(i+2, n-1):
-        if base[i]==base[i+1]=="(" and base[j]==base[j+1]==")":
-            cnt += 1
-print(cnt)
+result = 0
+for i in range(n-1,0,-1): #마지막인 0은 이미 1에서 같이 확인됨
+    if A[i] == ")" and A[i-1] == ")":
+        cnt += 1
+    elif A[i] =="(" and A[i-1]=="(":
+        result += cnt
+print(result)
