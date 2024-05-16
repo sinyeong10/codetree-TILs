76c44e65591db109a5c04aacff3f6b4d264158aa
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-05-16 11:32:28 +0900

    Title: 숨은 단어 찾기 2, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240516/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -0,0 +1,21 @@
+from sys import stdin
+n, m = list(map(int, stdin.readline().split()))
+base_2d = [stdin.readline().strip() for _ in range(n)]
+
+def in_range(i, j):
+    return 0<=i<n and 0<=j<m
+
+def check(i,j):
+    dxs, dys = [-1,1,0,0,-1,1,1,-1], [0,0,-1,1,1,1,-1,-1] #상하좌우,우상우하좌하좌상
+    tmp = 0
+    for dx, dy in zip(dxs, dys):
+        if in_range(i+dx, i+dy) and in_range(i+dx*2, j+dy*2) and base_2d[i+dx][j+dy] == base_2d[i+dx*2][j+dy*2] == "E":
+            tmp += 1
+    return tmp
+
+cnt=0
+for i in range(n):
+    for j in range(m):
+        if base_2d[i][j] == "L":
+            cnt += check(i,j)
+print(cnt)
