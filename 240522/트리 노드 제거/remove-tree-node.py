c504d7d027aca235949b89c0db426937369bd015
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-05-22 19:01:23 +0900

    Title: 트리 노드 제거, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240522/트리 노드 제거/remove-tree-node.py
+++ 240522/트리 노드 제거/remove-tree-node.py
@@ -14,14 +14,14 @@ for i in range(n):
 # print(graph)
 
 def count(node):
-    if node == del_node:
+    if node == del_node: #시작이 삭제되는 경우는 0
         return 0
     tmp = 0
     for elem in graph[node]:
-        if elem == del_node:
+        if elem == del_node: #요소 중 삭제된 건 건너뜀
             continue
-        tmp += count(elem)
-    if not graph[node] or graph[node] == [del_node]:
+        tmp += count(elem) #가능한 건 리프 숫자 세기 시작
+    if not graph[node] or graph[node] == [del_node]: #리프이거나, 아래 노드가 유일하며 삭제노드라면 카운트
         tmp += 1
     # print(node, tmp)
     return tmp
