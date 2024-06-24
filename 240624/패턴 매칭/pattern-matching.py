e3b657b52c298d0b0498c80081d58da6d245a487
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-06-24 15:24:19 +0900

    Title: 패턴 매칭, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240624/패턴 매칭/pattern-matching.py
@@ -0,0 +1,22 @@
+from sys import stdin
+s = stdin.readline().strip()
+p = stdin.readline().strip()
+
+def solve():
+    i_last = len(s)-1
+    j_last = len(p)-1
+    #뒤부터 채워가야  .*a와 같은 경우 처리 가능!
+    while j_last >= 0:
+        print(i_last, j_last)
+        if i_last < 0:
+            return False
+        if s[i_last] == p[j_last] or p[j_last] == ".":
+            i_last-=1
+            j_last-=1
+        elif p[j_last] == "*":
+            while i_last >= 0 and (p[j_last-1] == s[i_last] or p[j_last-1] == "."):
+                i_last -= 1
+            j_last -= 2
+    return True
+
+print("true" if solve() else "false")
