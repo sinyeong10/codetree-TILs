30021af749754a19bbca52fa4de1e77e41bb1e7a
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-10-02 15:59:46 +0900

    Title: 1차원 윷놀이, Time: 805ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 241002/1차원 윷놀이/yutnori-1d.py
@@ -0,0 +1,27 @@
+from sys import stdin
+n, m, k = list(map(int, stdin.readline().split()))
+action = list(map(int, stdin.readline().split()))
+
+move_map = [1 for _ in range(k)]
+
+ans = 0
+#k를 1개씩 n번 선택
+
+def back_track(cnt):
+    global ans
+    if cnt == n:
+        partical_ans = 0
+        for elem in move_map:
+            if elem >= m:
+                partical_ans += 1
+        ans = max(ans, partical_ans)
+        return
+    
+    for idx in range(k):
+        move_map[idx] += action[cnt]
+        back_track(cnt+1) #n-1까지 들어가고 n에서 체크함
+        move_map[idx] -= action[cnt]
+
+back_track(0)
+
+print(ans)
