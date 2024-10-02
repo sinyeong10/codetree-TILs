e4b9c45ac944ca90f1a235675cdedc1ee5f4a124
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-10-02 16:46:19 +0900

    Title: xor 결과 최대 만들기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241002/xor 결과 최대 만들기/max-of-xor.py
@@ -0,0 +1,21 @@
+from sys import stdin
+n, m = list(map(int, stdin.readline().split()))
+numbers = list(map(int, stdin.readline().split()))
+ans = -float("inf")
+pick_num = []
+
+def back_track(cnt, last_idx):
+    global ans
+    if cnt == m:
+        tmp = pick_num[0]
+        for i in range(1, m):
+            tmp ^= pick_num[i]
+        ans = max(ans, tmp)
+        return
+    for i in range(last_idx+1, n):
+        pick_num.append(numbers[i])
+        back_track(cnt+1, numbers[i])
+        pick_num.pop()
+
+back_track(0, -1)
+print(ans)
