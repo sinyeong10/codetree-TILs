401397d208a5453d384f6397c6cd8b64b397dc59
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-10-03 15:50:08 +0900

    Title: 마을 구분하기, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241003/마을 구분하기/seperate-village.py
@@ -0,0 +1,34 @@
+from sys import stdin
+n = int(stdin.readline())
+base_2d = [list(map(int, stdin.readline().split())) for _  in range(n)]
+visit_2d = [[False for _ in range(n)] for _ in range(n)]
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+def dfs(x,y):
+    global cnt
+    dxs, dys = [1,-1,0,0], [0,0,-1,1]
+    cnt += 1
+
+    for dx, dy in zip(dxs, dys):
+        next_x, next_y = x+dx, y+dy
+        if in_range(next_x, next_y) and not visit_2d[next_x][next_y] and base_2d[next_x][next_y] == 1:
+            visit_2d[next_x][next_y] = True
+            dfs(next_x, next_y)
+
+city = []
+
+for i in range(n):
+    for j in range(n):
+        if not visit_2d[i][j] and base_2d[i][j] == 1:
+            cnt = 0
+            visit_2d[i][j] = True
+            dfs(i,j)
+            city.append(cnt)
+            
+# print(city)
+city.sort()
+print(len(city))
+for elem in city:
+    print(elem)
