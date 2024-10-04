8e260ea67c22a01bee91a69abb693d0a132d4b87
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2024-10-04 15:48:54 +0900

    Title: 최대 증가 부분 수열, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 241004/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 241004/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -2,12 +2,12 @@ from sys import stdin
 n = int(stdin.readline())
 numbers = list(map(int, stdin.readline().split()))
 
-dp = [-1 for _ in range(n)]
-dp[0] = 1
+dp = [1 for _ in range(n)]
+# dp[0] = 1 #처음을 반드시 선택하지 않아도 됨
 
 for i in range(n): #dp[i] : i번째까지 왔을때 가장 긴 수열
     for j in range(i):
-        if numbers[j] < numbers[i] and dp[j] != -1: #무조건 증가해야함
+        if numbers[j] < numbers[i]: #무조건 증가해야함
             dp[i] = max(dp[i], dp[j]+1)
 
 print(max(dp))
