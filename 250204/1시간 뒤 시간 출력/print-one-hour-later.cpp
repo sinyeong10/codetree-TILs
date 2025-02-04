c033688d80e64ca7641b0e106ed4b4ab547a7581
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 13:58:54 +0900

    Title: 1시간 뒤 시간 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250204/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cout << a+1 << ":" << b;
+    return 0;
+}
