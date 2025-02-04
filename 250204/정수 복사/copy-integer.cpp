441eb5c1e01e2365674011132aac8c518ba9efdb
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 12:24:13 +0900

    Title: 정수 복사, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250204/정수 복사/copy-integer.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 3, b = 4;
+    b = a;
+
+    cout << a << " " << b << endl << a*b;
+    return 0;
+}
