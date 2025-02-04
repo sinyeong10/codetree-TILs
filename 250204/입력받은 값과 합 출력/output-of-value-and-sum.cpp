ea253657e55a9d452b15c4c9539c89ac9734ea3a
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 13:40:30 +0900

    Title: 입력받은 값과 합 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250204/입력받은 값과 합 출력/output-of-value-and-sum.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+    cout << a << " " << b << " " << a+b;
+    return 0;
+}
