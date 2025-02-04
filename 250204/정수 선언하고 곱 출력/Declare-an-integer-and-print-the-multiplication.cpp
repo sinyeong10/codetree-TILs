9e36618c7513c5cfa2aa6774acd29758964c5045
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 11:42:40 +0900

    Title: 정수 선언하고 곱 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250204/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 26, b = 5;
+    cout << a << " * " << b << " = " << a*b;
+    return 0;
+}
