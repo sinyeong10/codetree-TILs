14add926a1c70b54228dab011badcb0e98afb8d7
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 13:32:44 +0900

    Title: 실수 입력받아 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250204/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float a;
+    cin >> a;
+    cout.precision(2);
+    cout << fixed << a+1.5;
+    return 0;
+}
