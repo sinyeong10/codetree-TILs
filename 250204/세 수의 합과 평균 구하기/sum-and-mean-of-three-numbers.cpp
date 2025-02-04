db69e99023a43b78f916ce12799c15c44565fe40
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 14:32:36 +0900

    Title: 세 수의 합과 평균 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250204/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.cpp
+++ 250204/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    int a, b, c, d;
+    int a, b, c;
     cin >> a >> b >> c;
-    d = (double)(a+b+c)/3;
-    cout << a+b+c << endl << d;
+    // int d;
+    // d = (double)(a+b+c)/3; 근데 굳이 실수 연산 후 정수로 처리하지 않아도 됨!
+    cout << a+b+c << endl << (a+b+c)/3;
     return 0;
 }
