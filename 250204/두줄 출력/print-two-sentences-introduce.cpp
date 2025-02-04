0f690109399f1d2e739c5e3d11962111b00341dc
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 10:45:00 +0900

    Title: 두줄 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250204/두줄 출력/print-two-sentences-introduce.cpp
+++ 250204/두줄 출력/print-two-sentences-introduce.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
     // Please write your code here.
+    //cout을 2줄로 나눠 쓴다고 해서 줄개행이 포함되지는 않음!
     cout << "Hello students!" << endl << "Welcome to LeebrosCode!";
     return 0;
 }
