4bb85e3489ac7644add94f19c9d0f235955b40f7
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 11:40:49 +0900

    Title: 정수 선언하고 뺄셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250204/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 97, b = 13;
+    cout << a << " - " << b << " = " << a-b;
+    return 0;
+}
