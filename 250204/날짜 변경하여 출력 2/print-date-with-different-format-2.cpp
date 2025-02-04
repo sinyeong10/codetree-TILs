d6f6b06329826c0c5907f76b5bc43b653dd960aa
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 14:01:23 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250204/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int m, d, y;
+    cin >> m;
+    cin.get();
+    cin >> d;
+    cin.get();
+    cin >> y;
+    cout << y << "." << m << "." << d;
+    return 0;
+}
