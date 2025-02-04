3d7121dd53c30661be70adf7aee7c65d28a5db5e
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 13:39:23 +0900

    Title: 입력받은 값 교체하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250204/입력받은 값 교체하기/changing-inputs.cpp
+++ 250204/입력받은 값 교체하기/changing-inputs.cpp
@@ -5,9 +5,6 @@ int main() {
     // Please write your code here.
     int a, b, tmp;
     cin >> a >> b;
-    tmp = a;
-    a = b;
-    b = tmp;
-    cout << a << " " << b;
+    cout << b << " " << a;
     return 0;
 }
