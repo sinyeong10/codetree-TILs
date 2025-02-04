6b258e6784238a991a34f8acc2f8d1736a5f5799
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 12:09:59 +0900

    Title: 달에서 무게 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250204/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 13;
+    double b = 0.165;
+    cout << fixed;
+    cout.precision(6);
+    cout << a << " * " << b << " = " << a*b;
+
+    return 0;
+}
