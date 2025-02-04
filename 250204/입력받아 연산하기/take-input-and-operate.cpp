8c630e0ed7aedce0c141420d9ebf4f6a30814516
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 14:17:15 +0900

    Title: 입력받아 연산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250204/입력받아 연산하기/take-input-and-operate.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+    a += 87;
+    b %= 10;
+    cout << a << endl << b;
+    return 0;
+}
