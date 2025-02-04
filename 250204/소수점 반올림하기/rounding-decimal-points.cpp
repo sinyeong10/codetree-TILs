d1e492e275660c88bb3b15adea74e64ea0471537
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 12:10:55 +0900

    Title: 소수점 반올림하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250204/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a = 25.352;
+    cout << fixed;
+    cout.precision(1);
+    cout << a;
+    return 0;
+}
