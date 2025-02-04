54a3077c6375d40e518f10ce258c294e40570a33
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 14:39:30 +0900

    Title: 음수 구별하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250204/음수 구별하기/separate-negative-number.cpp
+++ 250204/음수 구별하기/separate-negative-number.cpp
@@ -6,9 +6,9 @@ int main() {
     int n;
     cin >> n;
     cout << n;
-    if (n < 0) cout << endl << "minus";
-    // if (n < 0) {
-    //     cout << endl << "minus";
-    // }
+    // if (n < 0) cout << endl << "minus";
+    if (n < 0) {
+        cout << endl << "minus";
+    }
     return 0;
 }
