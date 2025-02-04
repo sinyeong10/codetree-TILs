c864eded7ec8aba07aed0ea5a0069063f1bb33f9
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 13:33:32 +0900

    Title: 실수 입력받아 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250204/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
+++ 250204/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -6,7 +6,7 @@ int main() {
     float a;
     cin >> a;
     // cout.precision(2); #이렇게 먼저해도 되지만 의미가 유효숫자 설정이라서 아래처럼 하는 게 좀 더 명시적임
-    cout << fixed
+    cout << fixed;
     cout.precision(2);
     cout << a+1.5;
     return 0;
