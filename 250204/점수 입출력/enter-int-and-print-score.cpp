1819820d62b931cd8d517c9e29e4c2b1ebcab516
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 13:15:45 +0900

    Title: 점수 입출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250204/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    cout << "Your score is " << a << " point." << endl;
+    return 0;
+}
