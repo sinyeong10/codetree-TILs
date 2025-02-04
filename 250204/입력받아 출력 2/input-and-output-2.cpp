b88de365c5b6effc1cc93d3e6d3bfa204c4a0c7f
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 14:02:16 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250204/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cout << a << b;
+    return 0;
+}
