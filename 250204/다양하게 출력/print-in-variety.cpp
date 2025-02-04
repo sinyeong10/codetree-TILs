dcf0716a36f532de2309a7a842b5ec5d0a7bc8bd
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 10:50:28 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250204/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << "Total days in Year" << endl << 365 << endl;
+    cout << "Circumference rate" << endl << "3.1415926535" << endl;
+    return 0;
+}
