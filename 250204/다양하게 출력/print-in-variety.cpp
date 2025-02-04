2e91140610980b11ddf6e27a66b61cc1789acab3
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 11:10:57 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250204/다양하게 출력/print-in-variety.cpp
+++ 250204/다양하게 출력/print-in-variety.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
     // Please write your code here.
     cout << "Total days in Year" << endl << 365 << endl;
-    cout << "Circumference rate" << endl << "3.1415926535" << endl;
+    cout << fixed << setprecision(10); //<iomanip>가 필요함!, 소수점 아래 숫자수 고정!
+    cout << "Circumference rate" << endl << 3.1415926535 << endl;
     return 0;
 }
