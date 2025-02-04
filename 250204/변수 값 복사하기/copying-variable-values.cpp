ca706348c0d86849db348bb08346a36244d9d062
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-04 12:53:24 +0900

    Title: 변수 값 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250204/변수 값 복사하기/copying-variable-values.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 1, b = 2, c = 3;
+    a = b = c;
+    cout << a << " " << b << " " << c;
+    return 0;
+}
