6fad8312ad53a4433b0bdb2fb7acf6e16835601e
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-23 11:21:44 +0900

    Title: 정수의 조건 여부, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250223/정수의 조건 여부/numbers-condition.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    if (a >= 113) cout << 1 << endl;
+    else cout << 0 << endl;
+    return 0;
+}
