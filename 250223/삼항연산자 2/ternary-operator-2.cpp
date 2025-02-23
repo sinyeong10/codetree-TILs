f87f5041d3208fff015c129e73936ca851bc2318
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-23 11:45:15 +0900

    Title: 삼항연산자 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250223/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    cout << (a == 1 ? "t" : "f") << endl;
+    return 0;
+}
