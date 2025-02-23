21d8771f3e5514a438ff0e280484be29e2e1c049
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-02-23 11:44:14 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250223/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int score;
+    cin >> score;
+    cout << (score == 100 ? "pass" : "failure") << endl;
+    return 0;
+}
