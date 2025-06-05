f34af3f7e57ee13dc897e2e375f555c1c6326699
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-06-06 08:38:56 +0900

    Title: 변수 출력하기 3, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250606/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 1, b = 2;
+    char c = 'C';
+    cout << a << "->" << b << "->" << c;
+    return 0;
+}
