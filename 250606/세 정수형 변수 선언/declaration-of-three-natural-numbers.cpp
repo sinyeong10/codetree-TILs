4da9cb57c1b686ec415d6922c104b5c051531d94
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-06-06 08:39:55 +0900

    Title: 세 정수형 변수 선언, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250606/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 7, b = 23, c = 30;
+    cout << a << " + " << b << " = " << c;
+    return 0;
+}
