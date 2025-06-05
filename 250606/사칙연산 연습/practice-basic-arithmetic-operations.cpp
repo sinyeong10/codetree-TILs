c67b33a34c2cb79200afcb7752135a2e700ddb15
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-06-06 08:36:09 +0900

    Title: 사칙연산 연습, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250606/사칙연산 연습/practice-basic-arithmetic-operations.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 5, b = 30;
+    int c = a+b;
+    cout << c/8 << endl << c%8;
+    return 0;
+}
