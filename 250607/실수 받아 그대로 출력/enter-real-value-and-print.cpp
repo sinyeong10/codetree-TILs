cea9f273776d4f2782af069f9ec41120876ab49d
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-06-07 07:36:50 +0900

    Title: 실수 받아 그대로 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250607/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float N;
+    cin >> N;
+    cout << fixed;
+    cout.precision(2);
+    cout << N;
+    return 0;
+}
