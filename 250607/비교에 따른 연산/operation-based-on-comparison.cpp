9d38e3017b9b59274b0992800e7f6ab571611ac0
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-06-07 08:13:34 +0900

    Title: 비교에 따른 연산, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250607/비교에 따른 연산/operation-based-on-comparison.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+    if (a > b) {
+        cout << a*b;
+    } else {
+        cout << b/a;
+    }
+    return 0;
+}
