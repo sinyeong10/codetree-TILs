8c51b115b497637c6e5819823efcbd44af25c932
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-06-07 07:39:21 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250607/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float a, b, c;
+    cin >> a >> b >> c;
+    cout << fixed;
+    cout.precision(3);
+    cout << a << endl << b << endl << c;
+    return 0;
+}
