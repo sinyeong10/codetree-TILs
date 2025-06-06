eae24b273597c4f2814dd78d827539839d4dab39
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-06-07 08:04:26 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250607/비교 연산/comparison-operator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A, B;
+    cin >> A >> B;
+    cout << (A>=B) << endl << (A>B) << endl << (B>=A) << endl << (B>A) << endl << (A==B) << endl << (A!=B);
+    return 0;
+}
