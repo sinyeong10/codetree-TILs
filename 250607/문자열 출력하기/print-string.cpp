74e0a99243415f71a4419d1d571b5bc772e85300
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-06-07 09:08:06 +0900

    Title: 문자열 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250607/문자열 출력하기/print-string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++){
+        cout << "LeebrosCode" << endl;
+    }
+    return 0;
+}
