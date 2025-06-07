d984056130735d403cd405509ee24fe5be59b78a
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-06-07 09:05:56 +0900

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250607/문자 출력하기/print-text.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string s;
+    cin >> s;
+    for (int i = 0; i < 8; i++){
+        cout << s;
+    }
+    return 0;
+}
