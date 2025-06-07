e81f8f1bf95a032ff0abed1a69ba556764cdc805
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-06-07 09:13:29 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250607/장학금/scholarship.cpp
+++ 250607/장학금/scholarship.cpp
@@ -5,16 +5,12 @@ int main() {
     // Please write your code here.
     int midScore, finalScore;
     cin >> midScore >> finalScore;
-    if (midScore < 90){
-        cout << 0;
+    if (midScore >= 90 && finalScore >= 95){
+        cout << 100000;
+    } else if (midScore >= 90 && finalScore >= 90){
+        cout << 50000;
     } else {
-        if (finalScore >= 95){
-            cout << 100000;
-        } else if (finalScore >= 90){
-            cout << 50000;
-        } else {
-            cout << 0;
-        }
+        cout << 0;
     }
     return 0;
 }
