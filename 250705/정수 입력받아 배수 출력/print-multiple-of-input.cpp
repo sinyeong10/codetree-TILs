15c44e43ea3aeba68c4e5e9964a9998abbeab6b5
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-07-05 15:03:11 +0900

    Title: 정수 입력받아 배수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250705/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+    for (int i=1;i<=5;i++){
+        cout << N*i << " ";
+    }
+    return 0;
+}
