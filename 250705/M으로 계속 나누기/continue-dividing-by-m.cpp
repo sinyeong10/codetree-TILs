e58106aa929e0293a7d00523fe7caf537452a759
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-07-05 16:15:32 +0900

    Title: M으로 계속 나누기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250705/M으로 계속 나누기/continue-dividing-by-m.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N, M;
+
+    cin >> N;
+    cin >> M;
+    
+    // Please write your code here.
+    while (N != 0){
+        cout << N << endl;
+        N /= M;
+    }
+    
+    return 0;
+}
