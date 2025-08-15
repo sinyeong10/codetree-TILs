5e197a3739ece2641a2670df6325b27540287a0c
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-08-15 19:27:56 +0900

    Title: a로 채워넣기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250815/a로 채워넣기/filling-with-a.cpp
+++ 250815/a로 채워넣기/filling-with-a.cpp
@@ -6,6 +6,7 @@ int main() {
     string A;
     cin >> A;
     A[1] = 'a';
+    //인덱스는 0~A.length()-1까지 길이는 A.length(), 따라서 뒤에서 2번째는 -2가 맞음
     A[A.length()-2] = 'a';
     cout << A;
     return 0;
