40a976959a7947b9852ffbacb4a176d07c52f837
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-08-15 18:09:29 +0900

    Title: 배열 채우고 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250815/배열 채우고 출력하기/filling-array-and-print.cpp
+++ 250815/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -4,12 +4,11 @@ using namespace std;
 int main() {
     char A[10];
     for (int i=0;i<sizeof(A)/sizeof(char);i++){
-        cout << i << ", ";
         cin >> A[i];
     }
 
-    for (int i=sizeof(A)/sizeof(char)-1;i>0;i--){
-        cout << A[i] << " ";
+    for (int i=sizeof(A)/sizeof(char)-1;i>=0;i--){
+        cout << A[i];
     }
     // Please write your code here.
     return 0;
