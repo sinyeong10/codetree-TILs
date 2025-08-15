ef6a33c1aae76f075509f49ceb359b463cad4c25
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-08-15 20:22:38 +0900

    Title: 0 나올때까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250815/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    while (true) {
+        cin >> n;
+        if (n == 0){
+            break;
+        }
+        cout << n << "\n";
+    }
+    return 0;
+}
