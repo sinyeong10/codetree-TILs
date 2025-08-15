b94615f5b88c3c5989ccca9486d60e5b9fa2b375
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-08-15 19:08:05 +0900

    Title: 따옴표 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250815/따옴표 출력/print-quote.cpp
+++ 250815/따옴표 출력/print-quote.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    cout << "He says \"It\'s a really simple sentence\"."
+    cout << "He says \"It\'s a really simple sentence\".";
     return 0;
 }
