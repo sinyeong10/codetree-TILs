10cb57d1611536105695f7d4c1ed55da921e2d2f
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-08-17 19:36:06 +0900

    Title: 숫자 피라미드, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250817/숫자 피라미드/number-pyramid.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+    for (int i=1;i<=N;i++){
+        for (int j=0;j<i;j++){
+            cout << i << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
