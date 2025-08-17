674812f0dd8ee530a891465ec6f52a0e52650b18
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-08-17 19:33:54 +0900

    Title: 정상적인 구구단, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250817/정상적인 구구단/normal-multiple.cpp
+++ 250817/정상적인 구구단/normal-multiple.cpp
@@ -6,10 +6,12 @@ int main() {
     int N;
     cin >> N;
     for (int i=1;i<=N;i++){
-        for (int j=1;j<N;j++){
-            cout << i << " * " << j << " = " << i*j << ", ";
+        for (int j=1;j<=N;j++){
+            cout << i << " * " << j << " = " << i*j;
+            if (j!=N){
+                cout << ", ";
+            }
         }
-        cout << i << " * " << N << " = " << i*N;
         cout << "\n";
     }
     return 0;
