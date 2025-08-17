9a8623daf6232ad87093fed8ddd5183efc7bf39c
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-08-17 20:36:47 +0900

    Title: 두 개의 격자 비교하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250817/두 개의 격자 비교하기/compare-two-grid.cpp
+++ 250817/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -5,7 +5,7 @@ int main() {
     // Please write your code here.
     int N, M;
     cin >> N >> M;
-    int arr1_2d[N][M], arr2_2d[N][M], arr3_2d[N][M]; //={0,}로 모두 0으로 초기화 불가능??
+    int arr1_2d[N][M], arr2_2d[N][M], arr3_2d[N][M]={}; //={0}, ={0,}로 모두 0으로 초기화 불가능??
     for (int i=0;i<N;i++){
         for (int j=0;j<M;j++){
             cin >> arr1_2d[i][j];
@@ -20,9 +20,9 @@ int main() {
         for (int j=0;j<M;j++){
             if (arr1_2d[i][j] != arr2_2d[i][j]){
                 arr3_2d[i][j] = 1;
-            } else {
-                arr3_2d[i][j] = 0;
-            }
+            }//  else {
+            //     arr3_2d[i][j] = 0;
+            // }
         }
     }
     for (int i=0;i<N;i++){
