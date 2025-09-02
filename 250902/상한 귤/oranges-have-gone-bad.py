828c579385e9e92c7cbbb95b58602ab47a30009d
V. hs Sin <58167199+sinyeong10@users.noreply.github.com>
2025-09-02 22:34:52 +0900

    Title: 상한 귤, Time: 99ms, Memory: 20MB, Status: Passed - Codetree

--- 250902/상한 귤/oranges-have-gone-bad.py
+++ 250902/상한 귤/oranges-have-gone-bad.py
@@ -4,20 +4,15 @@ n, k = map(int, input().split())
 step = [list(map(int, input().split())) for _ in range(n)]
 
 q = deque()
-visited = [
-    [False for _ in range(n)]
-    for _ in range(n)
-]
 
 def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < n
 
 def can_go(x, y):
-    return in_range(x, y) and step[x][y] == -2 and not visited[x][y]
+    return in_range(x, y) and step[x][y] == -2
 
 def push(new_x, new_y, new_step):
     q.append((new_x, new_y))
-    visited[new_x][new_y] = True
     step[new_x][new_y] = new_step
     
 for i in range(n):
